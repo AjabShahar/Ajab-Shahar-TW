16b5ef364bc7029d7e5b4a82f461897f9e00b00d
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-09-08 00:16:50 +0530

    fixing the autoplay of youtube.

--- AjabShahar/public/js/app/services/cmsService.js
+++ AjabShahar/public/js/app/services/cmsService.js
@@ -1,11 +1,11 @@
 var cmsService = function ($http) {
   var getScreenOptions = function () {
-    return 	                 {
-                                    "options" :[
-                                                {"format":"audio","url":"https://www.youtube.com/embed/O-WVDBpBdRY?enablejsapi=1","imageUrl":""},
-                                                {"format":"video","url":"https://www.youtube.com/embed/O-WVDBpBdRY?enablejsapi=1"}
-                                    ]
-                                };
+    return {
+        "options" :[
+                    {"format":"audio","url":"https://www.youtube.com/embed/O-WVDBpBdRY?enablejsapi=1","imageUrl":""},
+                    {"format":"video","url":"https://www.youtube.com/embed/O-WVDBpBdRY?enablejsapi=1"}
+        ]
+    };
   };
 
   return {
--- AjabShahar/public/js/app/utils/youtube_player.js
+++ AjabShahar/public/js/app/utils/youtube_player.js
@@ -1,23 +1,102 @@
-// create youtube player
-var player;
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('player', {
-    videoId: '0Bmhjf0rKe8',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
+introductionApp.constant('YT_event', {
+  STOP:            0,
+  PLAY:            1,
+  PAUSE:           2
+});
+
+introductionApp.controller('YouTubeCtrl', function($scope, YT_event) {
+  $scope.yt = {
+    width: '100%',
+    height: '100%',
+    videoid: "M7lc1UVf-VE"
+  };
+});
+
+introductionApp.directive('youtube', function($window, YT_event) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "@"
+    },
+
+    template: '<div></div>',
+
+    link: function(scope, element, attrs, $rootScope) {
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      $window.onYouTubeIframeAPIReady = function() {
+
+        player = new YT.Player(element.children()[0], {
+          playerVars: {
+            autoplay: 0,
+            html5: 1,
+            theme: "light",
+            modesbranding: 0,
+            color: "white",
+            iv_load_policy: 3,
+            showinfo: 1,
+            controls: 1
+          },
+
+          height: scope.height,
+          width: scope.width,
+          videoId: scope.videoid,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          },
+        });
       }
-    });
-}
 
-// autoplay video
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
 
-// when video ends
-function onPlayerStateChange(event) {
-    if(event.data === 0) {
-      event.target.playVideo();
+        // when video ends
+        function onPlayerStateChange(event) {
+            if(event.data === 0) {
+              event.target.playVideo();
+            }
+        }
+      scope.$watch('height + width', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        player.setSize(scope.width, scope.height);
+
+      });
+
+      scope.$watch('videoid', function(newValue, oldValue) {
+        if (newValue == oldValue) {
+          return;
+        }
+
+        player.cueVideoById(scope.videoid);
+
+      });
+
+      scope.$on(YT_event.STOP, function () {
+        player.seekTo(0);
+        player.stopVideo();
+      });
+
+      scope.$on(YT_event.PLAY, function () {
+        player.playVideo();
+      });
+
+      scope.$on(YT_event.PAUSE, function () {
+        player.pauseVideo();
+      });
+
     }
-}
+  };
+});
