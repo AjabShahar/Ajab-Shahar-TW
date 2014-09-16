1e6cfc75b1a49e79b8cbed67e505ffddb2a64fb8
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-09-16 14:36:31 +0530

    fixing tests

--- AjabShahar/public/js/app/directives/content/image.js
+++ AjabShahar/public/js/app/directives/content/image.js
@@ -12,11 +12,11 @@ contentModule.directive("imageWithDetails", function() {
         },
         templateUrl:'js/templates/content/image.html',
         controller:function($scope){
-            $scope.shift = function(direction,num){
-                if(direction && (direction=='r'||direction=='R'))
-                    return 'shiftRight'+num;
-                if(direction && (direction=='l'||direction=='L'))
-                    return 'shiftLeft'+num;
+            $scope.shift = function(){
+                if($scope.shiftDirection && ($scope.shiftDirection=='r'||$scope.shiftDirection=='R'))
+                    return 'shiftRight'+$scope.shiftIndex;
+                if($scope.shiftDirection && ($scope.shiftDirection=='l'||$scope.shiftDirection=='L'))
+                    return 'shiftLeft'+$scope.shiftIndex;
             };
         }
     }
