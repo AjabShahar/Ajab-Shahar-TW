8aaef0a2239eb4a9b5027794fdf15c0cbd802a21
Jaideep Rana <jaideepk@thoughtworks.com>
2014-09-18 12:54:28 +0530

    Refactoring code for lightbox on main-landing page.<Jaideep/Soumya>

--- AjabShahar/public/js/app/directives/content/song.js
+++ AjabShahar/public/js/app/directives/content/song.js
@@ -8,6 +8,17 @@ contentModule.directive("songWithDetails", function() {
             singer:'@',
             imgSrc:'@',
             customStyle:'@',
+            lightBoxImageUrl:'@',
+        },
+        controller: function($scope) {
+            $scope.showLightBox = function(){
+
+            }
+        },
+        link: function($scope,element, attrs){
+//                    $(".group2").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+            var colorBoxConfig = {rel:'group1', arrowKey: false};
+            $(".group1").colorbox(colorBoxConfig);
         },
         templateUrl:'js/templates/content/song.html',
     }
