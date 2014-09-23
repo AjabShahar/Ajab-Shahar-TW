fa7675e22c6adf9ec0bec7a91de2d26628356a31
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-09-23 16:19:05 +0530

    example of the pop up

---
+++ AjabShahar/public/js/app/directives/popupSupport/popup.js
@@ -0,0 +1,35 @@
+document.createElement('pop-up');
+angular.module('PopupSupport', [])
+    .directive('popUp', function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            replace: true,
+            scope: {
+                show: '&', //true/false - displays popup
+                onClose: '&', //close click handler ~ callback to controller
+                closeButton: '@', //true-false ~ displays/hides close option
+                width: '@', //width of the popup
+                overlayId: '@'//an id for the background overlay for manipulation via jquery
+            },
+            templateUrl: 'js/templates/popupSupport/popup.html',
+            controller: function($scope) {
+                $scope.$watch(function() { return $scope.show(); }, function(newValue, oldValue) {
+                    if (newValue != true)
+                        return;
+
+                    if ($scope.overlayId) {
+                        $('#' + $scope.overlayId).css("height", overlayHeight());
+                    }
+                });
+
+                function overlayHeight() {
+                    if ($(document).height() > $(window).height()) {
+                        return $(document).height() + 40;
+                    } else {
+                        return $(window).height();
+                    }
+                };
+            }
+        };
+    });    
