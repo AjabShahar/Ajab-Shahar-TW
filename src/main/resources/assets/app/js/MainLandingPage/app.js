d0d076aa3324cef4c34b042fbf36f2e7d4d3982e
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-09-24 13:02:40 +0530

    merging the popup box

--- src/main/resources/assets/app/js/MainLandingPage/app.js
+++ src/main/resources/assets/app/js/MainLandingPage/app.js
@@ -1,4 +1,4 @@
-var mainLandingPageApp = angular.module('mainLandingPageApp',['angular-parallax','contentModule','youtubeApp']);
+var mainLandingPageApp = angular.module('mainLandingPageApp',['angular-parallax','thumbnailModule','youtubeApp','PopupSupport']);
 
 mainLandingPageApp.config(function($sceDelegateProvider) {
                         $sceDelegateProvider.resourceUrlWhitelist([
