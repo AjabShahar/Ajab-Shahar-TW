d1959203aaae701c4fe29b2e2c90a540cac398b0
Jaideep Rana <jaideepk@thoughtworks.com>
2014-09-29 11:14:40 +0530

    WIP for fixing CI-deploy task.<Jaideep/Padma>

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,3 @@
 cat as_pid_file | xargs kill -9
-java -jar platform-1.0-SNAPSHOT.jar server $1 > server.log &
+nohup java -jar target/platform-1.0-SNAPSHOT.jar server $1 > server.log &
 echo $! > as_pid_file
