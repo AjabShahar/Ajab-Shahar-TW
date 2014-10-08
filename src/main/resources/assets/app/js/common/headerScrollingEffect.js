ba0a6dc2fefe40d0b0f8ad1d88ecb37e8ccd898c
padmavb <padmavb@INpadmavb.local>
2014-10-08 19:04:09 +0530

    Fixed header art problem

---
+++ src/main/resources/assets/app/js/common/headerScrollingEffect.js
@@ -0,0 +1,25 @@
+
+$(function(){
+$(window).scroll(function(){
+  var sticky = $('.headerWrapper'),stickyArt = $('.headerArt'),
+      scroll = $(window).scrollTop(),toFix=$('.headerWrapper').height();
+
+  if (scroll >= toFix){sticky.addClass('fixed');stickyArt.addClass('fixed');$('body').css('padding-top',toFix+'px');}
+  else {sticky.removeClass('fixed');stickyArt.removeClass('fixed');$('body').css('padding-top','0px');}
+});
+
+
+$(document).on('click','div.headerArt',function(){
+
+var addMarginToArt=$('.headerWrapper').height();
+if($('.headerWrapper').css('display')=='none'){
+    $('.headerWrapper').addClass('fixedOnScroll');
+    $('.headerArt').css('top',addMarginToArt+'px');
+}
+else{
+    $('.headerWrapper').removeClass('fixedOnScroll');
+    $('.headerArt').css('top','0px');
+}
+});
+
+});
