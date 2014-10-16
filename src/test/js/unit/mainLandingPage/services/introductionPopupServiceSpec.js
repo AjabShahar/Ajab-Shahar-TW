0eb173c7c465f3cba8f4dd653da6192304f417cb
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-10-16 19:22:31 +0530

    added the introductionPopupServiceSpec

---
+++ src/test/js/unit/mainLandingPage/services/introductionPopupServiceSpec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe("introduction popup service Specs", function(){
+	var service;
+
+    beforeEach(module('htmlGenerator'));
+
+    beforeEach(inject(function(introductionPopupService) {
+        service = introductionPopupService;
+    }));
+
+    it("should get popup for songs", function(){
+        var songsSampleResponse =
+                [
+                    {
+                        "category": "Songs",
+                        "categoryName": "Song & Reflection",
+                        "name": "Practice the art of dying",
+                        "poet": "Sharath",
+                        "youtubeVideoId": "videoId",
+                        "imageUrl": "imgId",
+                        "singer": "Parvathy Baul",
+                        "duration": "09:11"
+                    }
+                ]
+        ;
+
+        var landingPageThumbnails = service.getPopupDetails(songsSampleResponse);
+
+        expect(landingPageThumbnails).toBe('<pop-up id="oid0" width="100" show="shouldShow(\'oid0\')" on-close="onClose(\'oid0\')" closeButton="true">'+
+            '<song-introduction singer="Parvathy Baul" name="Practice the art of dying" url="https://www.youtube.com/embed/videoId"></song-introduction></pop-up>');
+    });
+
+});
