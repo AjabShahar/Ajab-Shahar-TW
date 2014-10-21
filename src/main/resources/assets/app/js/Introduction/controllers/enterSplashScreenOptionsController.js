bb482fb8c0cd2f5df3e29fef5fd49be209170949
Jaideep Rana <jaideepk@thoughtworks.com>
2014-10-21 15:37:55 +0530

    Refactored the splashScreenOptions for admin. <Jaideep>

--- src/main/resources/assets/app/js/Introduction/controllers/enterSplashScreenOptionsController.js
+++ src/main/resources/assets/app/js/Introduction/controllers/enterSplashScreenOptionsController.js
@@ -1,21 +1,3 @@
-//var enterSplashScreenOptionsController = function($scope,contentService){
-//   $scope.formInfo = {};
-//   $scope.test = {'name': 'test123'};
-//   $scope.saveData = function() {
-//         alert("hello");
-//   };
-//
-//   contentService.postData(data);
-// };
-//
-//introductionApp.controller('enterSplashScreenOptionsController',['$scope','contentService',enterSplashScreenOptionsController]);
-//
-//var postData = function(data, $http){
-//   $http.post('/SplashScreenOptions',data).success(function(){
-//      alert("Data added");
-//   });
-// };
-
 var enterSplashScreenOptionsController = function($scope, $http){
 
   $scope.formInfo = {};
