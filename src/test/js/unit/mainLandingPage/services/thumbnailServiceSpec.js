8d9f5065a48d7170e9829465df01554af1cafbd4
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-10-21 16:30:01 +0530

    fixing test

--- src/test/js/unit/mainLandingPage/services/thumbnailServiceSpec.js
+++ src/test/js/unit/mainLandingPage/services/thumbnailServiceSpec.js
@@ -46,10 +46,10 @@ describe("Thumbnail service Specs", function(){
             ' img-src="imgId"'+
             ' url="videoId"'+
             ' name="Practice the art of dying"'+
-             //singer="Parvathy Baul"
             ' category-name="Song & Reflection"'+
             ' duration="09:11"'+
-            //' poet="Sharath">
+            ' singer="Parvathy Baul"'+
+            ' poet="Sharath">'+
             '</song-with-details>');
 	});
 
