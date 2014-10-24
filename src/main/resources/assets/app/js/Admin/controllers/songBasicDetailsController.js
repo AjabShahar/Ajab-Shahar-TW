e1b1f52951d5b88cf0bef6da3b625fc7c316f539
Jaideep Rana <jaideepk@thoughtworks.com>
2014-10-24 16:12:23 +0530

    POST functionality complete for song landing page. <Jaideep>

--- src/main/resources/assets/app/js/Admin/controllers/songBasicDetailsController.js
+++ src/main/resources/assets/app/js/Admin/controllers/songBasicDetailsController.js
@@ -1,21 +1,26 @@
 var songBasicDetailsController = function($scope, $http){
 
   $scope.formInfo = {};
-  $scope.formInfo.singers=new Array();
-  $scope.formInfo.poets=[];
-  $scope.singersList=[];
-  $scope.poetsList=[];
+  $scope.formInfo.singers = [];
+  $scope.formInfo.poets = [];
+  $scope.category = {};
+  $scope.singersList = [];
+  $scope.poetsList = [];
+  $scope.categoryList = [];
+
   $http.get('/api/people/singers').success(function(singersList){
-                   $scope.singersList = singersList;
-
+    $scope.singersList = singersList;
   });
-  $http.get('/api/people/poets').success(function(poetsList){
-                     $scope.poetsList = poetsList;
 
+  $http.get('/api/people/poets').success(function(poetsList){
+    $scope.poetsList = poetsList;
+  });
+
+  $http.get('/api/category').success(function(categoryList){
+    $scope.categoryList = categoryList;
   });
 
   $scope.saveData = function(){
-    $scope.formInfo.category = {"name" : "Song"};
     $http.post('/api/songs',$scope.formInfo).success(function(){
             alert("Data added");
          });
