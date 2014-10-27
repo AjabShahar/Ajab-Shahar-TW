488df41229c8012975370a159593385a17c19675
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-10-27 21:18:16 +0530

    carousel directive added

--- src/main/resources/assets/app/js/common/directives/carouselSupport/carousel.js
+++ src/main/resources/assets/app/js/common/directives/carouselSupport/carousel.js
@@ -2,28 +2,60 @@ var carouselModule = angular.module('carouselModule', ['ngAnimate']);
 
 carouselModule.directive('carousel', function($timeout) {
   return {
-    restrict: 'AE',
+    restrict: 'E',
+    transclude: true,
     replace: true,
-    scope: {
-      elements: '='
-    },
+    scope: {},
+    controller: ['$scope',function($scope) {
+      $scope.currentIndex = 0; // Initially the index is at the first element
+      $scope.sections = [];
+      $scope.carouselSelection = 'one';
+
+      $scope.next = function() {
+          var oldIndex = $scope.currentIndex;
+          $scope.currentIndex < $scope.sections.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+          $scope.sections[oldIndex].selected = false;
+          $scope.sections[$scope.currentIndex].selected = true;
+      };
+
+      $scope.prev = function() {
+          var oldIndex = $scope.currentIndex;
+          $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.sections.length - 1;
+          $scope.sections[oldIndex].selected = false;
+          $scope.sections[$scope.currentIndex].selected = true;
+      };
+
+      this.addSection = function(section) {
+        $scope.sections.push(section);
+        if($scope.sections.length==1)
+        {
+            $scope.currentIndex = 0;
+            $scope.sections[$scope.currentIndex].selected = true;
+        }
+      }
+    }],
     link: function(scope, elem, attrs) {
-	scope.currentIndex = 0; // Initially the index is at the first element
-
-	scope.next = function() {
-        var oldIndex = scope.currentIndex;
-        scope.currentIndex < scope.elements.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
-        scope.elements[oldIndex].visible = false;
-        scope.elements[scope.currentIndex].visible= true;
-	};
-
-	scope.prev = function() {
-        var oldIndex = scope.currentIndex;
-        scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.elements.length - 1;
-        scope.elements[oldIndex].visible = false;
-        scope.elements[scope.currentIndex].visible= true;
-	};
     },
     templateUrl:'/js/common/templates/carouselSupport/carousel.html',
   };
-});
+})
+.directive('section', function() {
+     return {
+       require: '^carousel',
+       restrict: 'E',
+       transclude: true,
+       scope: { id: '@' },
+       link: function(scope, element, attrs, carouselCtrl) {
+         scope.selected = false;
+         scope.isSelected = function(){
+            return scope.selected;
+         }
+
+         carouselCtrl.addSection(scope);
+       },
+       template:
+       '<div ng-init="selected=false" ng-show="isSelected()" class="carousel-animation" ng-transclude>'+
+        '</div>',
+       replace: true
+     };
+   });
--- src/main/resources/assets/app/js/songs/controllers/allSongsController.js
+++ src/main/resources/assets/app/js/songs/controllers/allSongsController.js
@@ -1,26 +1,6 @@
 var allSongsController = function($scope,contentService,songThumbnailService,introductionPopupService,popupService){
     $scope.popupService = popupService;
-    $scope.elements = [{
-        src: 'img1.png',
-        title: 'Pic 1',
-        visible:true
-    }, {
-        src: 'img2.jpg',
-        title: 'Pic 2',
-        visible:false
-    }, {
-        src: 'img3.jpg',
-        title: 'Pic 3',
-        visible:false
-    }, {
-        src: 'img4.png',
-        title: 'Pic 4',
-        visible:false
-    }, {
-        src: 'img5.png',
-        title: 'Pic 5',
-        visible:false
-    }];
+
 };
 
 allSongsApp.controller('allSongsController',['$scope','contentService','songThumbnailService','introductionPopupService','popupService',allSongsController]);
