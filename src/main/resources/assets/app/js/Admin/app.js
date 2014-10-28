3045f2820bb816d82a4b2ae841921fdc219ea786
padmavb <padmavb@INpadmavb.local>
2014-10-28 19:14:58 +0530

    Partial work on editing song details

--- src/main/resources/assets/app/js/Admin/app.js
+++ src/main/resources/assets/app/js/Admin/app.js
@@ -1,1 +1,9 @@
-var adminApp = angular.module('adminApp',[]);
+var adminApp = angular.module('adminApp',['ngRoute']);
+
+adminApp.config(['$routeProvider',
+function($routeProvider) {
+     $routeProvider.
+       when('/edit/:id', {
+         templateUrl: 'partials/songDetails/edit.html'
+       });
+   }]);
