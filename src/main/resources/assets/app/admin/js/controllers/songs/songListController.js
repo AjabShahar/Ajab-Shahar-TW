c71be82ad09e1b4485c517e09d942a778c059542
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-10-31 10:25:15 +0530

    added NameService to list.html

--- src/main/resources/assets/app/admin/js/controllers/songs/songListController.js
+++ src/main/resources/assets/app/admin/js/controllers/songs/songListController.js
@@ -3,14 +3,21 @@ var songListController = function($scope, contentService,nameService){
     $scope.nameService = nameService;
     $scope.init = function(){
         contentService.getAllSongs().then(function(result){
-            $scope.songs = result.data;
+            var allSongs = result.data;
+            $scope.songs = _.reduce(allSongs,function(songs, value,index) {
+                var toBeAdded={};
+                toBeAdded.title = value.englishTranslation;
+                toBeAdded.categoryName = value.songCategory.name;
+                toBeAdded.singerNames = _.reduce(value.singers, function(memo, value, index){ return ((index!=0)?' ,':'')+ memo+ nameService.getName(value); },'');
+                toBeAdded.poetNames = _.reduce(value.poets, function(memo, value, index){ return ((index!=0)?' ,':'')+ memo+ nameService.getName(value); },'');
+                toBeAdded.id = value.id;
+                songs.push(toBeAdded);
+                return songs;
+            },[])
         });
     }
 
     $scope.init();
-    $scope.getName=function(person){
-        return $scope.nameService.getName(person);
-    }
 }
 
 adminApp.controller('songListController',['$scope','contentService','nameService',songListController]);
