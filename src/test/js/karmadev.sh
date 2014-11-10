4047a6053d4ae907fa86fb5e6ed75c5ff1168e76
Jaideep Rana <jaideepk@thoughtworks.com>
2014-11-10 14:49:13 +0530

    Updating the required files. <Jaideep>

--- src/test/js/karmadev.sh
+++ src/test/js/karmadev.sh
@@ -4,4 +4,4 @@ echo ""
 echo "Starting Karma Server (http://karma-runner.github.io)"
 echo "-------------------------------------------------------------------"
 
-../../main/resources/assets/node_modules/karma/bin/karma start ../../test/js/config/karma.dev.conf.js $*
+../../../node_modules/karma/bin/karma start ../../test/js/config/karma.dev.conf.js $*
