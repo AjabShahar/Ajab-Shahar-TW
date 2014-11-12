3fc92939bdbe7d52f760140e479280f89d8eb6bd
Jaideep Rana <jaideepk@thoughtworks.com>
2014-11-12 15:08:40 +0530

    WIP for filtering based on alphabets. <Jaideep>

--- src/main/resources/assets/app/user/js/songs/app.js
+++ src/main/resources/assets/app/user/js/songs/app.js
@@ -1,7 +1,5 @@
 var allSongsApp = angular.module('allSongsApp',['thumbnailModule','mediaPlayer','popupSupport','htmlGenerator','sticky', 'headerModule','carouselModule', 'infinite-scroll']);
 
-
 allSongsApp.config(resourceUrlWhiteList);
 allSongsApp.factory('songsContentService', ['$http', songsContentService]);
-
 allSongsApp.directive('bindDynamicHtml', ['$compile', bindDynamicHtml]);
--- src/main/resources/assets/app/user/js/songs/controllers/allSongsController.js
+++ src/main/resources/assets/app/user/js/songs/controllers/allSongsController.js
@@ -3,7 +3,10 @@ var allSongsController = function($scope,songsContentService,songThumbnailServic
     $scope.songDetails=[];
     $scope.totalSongs = null;
     $scope.detailsService = popupService;
-    var i = 0, j;
+    $scope.activeLetter = 'f';
+    $scope.filterOn = true;
+
+    var i = 0;
 
     $scope.getTotalSongsCount = function(){
         songsContentService.getAllSongs().then(function(songsList){
@@ -11,15 +14,41 @@ var allSongsController = function($scope,songsContentService,songThumbnailServic
         });
     }
 
-    $scope.loadSongFromRange = function(from , to){
-    	songsContentService.getSongsGivenRange(from, to).then(function(result){
-            for(j = 0; j < result.data.length ;j++, i++){
-                var songThumbnailWithBubble = songThumbnailService.getThumbnailWithBubble(result.data[j],'song_'+result.data[j].id,'');
+    $scope.filterBasedOnLetter = function(input, letter){
+        input = input || [];
+        var out = [];
+        input.forEach(function (item) {
+            if (item.songTitle.englishTranslation.charAt(0).toLowerCase() == letter) {
+                out.push(item);
+            }
+        });
+        return out;
+    }
+
+    $scope.createSongThumbnailWithPopUp = function(result){
+        var j;
+        for(j = 0; j < result.length ;j++, i++){
+                var songThumbnailWithBubble = songThumbnailService.getThumbnailWithBubble(result[j],'song_'+result[j].id,'');
+
                 $scope.songs.push(songThumbnailWithBubble);
 
-                var popupContent = songThumbnailService.getPopupDetails(result.data[j],j,'song_'+result.data[j].id,j);
+                var popupContent = songThumbnailService.getPopupDetails(result[j],j,'song_'+result[j].id,j);
                 $scope.songDetails.push(popupContent);
             }
+    }
+
+    $scope.loadSongFromRange = function(from , to){
+    	songsContentService.getSongsGivenRange(from, to).then(function(result){
+            var filteredResult = null;
+
+            if(!$scope.filterOn){
+                filteredResult = $scope.filterBasedOnLetter(result.data, $scope.activeLetter);
+            }
+            else {
+                filteredResult = result.data;
+            }
+
+            $scope.createSongThumbnailWithPopUp(filteredResult);
         });
     }
 
