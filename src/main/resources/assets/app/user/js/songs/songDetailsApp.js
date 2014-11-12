ed5f57d8cb033f1cb98e4318b4fbac3593456a89
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-11-12 11:21:51 +0530

    getting bootstrap using bower

--- src/main/resources/assets/app/user/js/songs/songDetailsApp.js
+++ src/main/resources/assets/app/user/js/songs/songDetailsApp.js
@@ -1,4 +1,4 @@
-var songDetailsApp = angular.module('allSongsApp',['thumbnailModule','mediaPlayer','htmlGenerator','sticky', 'headerModule','carouselModule']);
+var songDetailsApp = angular.module('allSongsApp',['thumbnailModule','mediaPlayer','htmlGenerator','sticky', 'headerModule','carouselModule','accordionModule']);
 
 songDetailsApp.config(resourceUrlWhiteList);
 songDetailsApp.factory('songsContentService', ['$http', songsContentService]);
