d85806e9f15ab6b1207662cad44b7b084f88bcf3
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-11-13 13:17:10 +0530

    added songIntroduction Mapper Spec

---
+++ src/test/js/unit/directives/mappers/introduction/songInroductionMapperSpec.js
@@ -0,0 +1,63 @@
+'use strict';
+
+describe('Mapper', function() {
+
+    describe("song Mapper Specs", function(){
+        var service;
+        var scope;
+        var element;
+        var compile;
+        var template;
+
+        beforeEach(function(){
+            module('htmlGenerator');
+            module('thumbnailModule');
+        });
+
+        beforeEach(inject(function($rootScope,$compile,$templateCache) {
+            scope = $rootScope.$new();
+            compile = $compile;
+            template = $templateCache;
+        }));
+
+        it("should get popup for songs", function(){
+            scope.detailsFromService=
+                    {
+                        "id":2,
+                        "contentType": "Songs",
+                        "categoryName": "Song & Reflection",
+                        "songTitle":{
+                            "englishTranslation": "Practice the art of dying",
+                         },
+                        "poets": [
+                            {
+                            "category": "POET",
+                            "firstName": "Sharath",
+                            "middleName": "",
+                            "lastName": ""
+                            }
+                        ],
+                        "youtubeVideoId": "videoId",
+                        "soundCloudTrackID": "scId",
+                        "imageUrl": "imgId",
+                        "singers": [
+                            {
+                            "category": "SINGER",
+                            "firstName": "Parvathy",
+                            "middleName": "",
+                            "lastName": "Baul"
+                            }
+                            ],
+                        "duration": "09:11"
+                    };
+
+            element = angular.element('<song-introduction-mapper details="detailsFromService" show-details-service="detailsService"/>');
+            template.put('/user/js/common/templates/mappers/introduction/songIntroductionMapper.html', '{{song.id}} {{song.singer}} {{song.poet}} {{song.title}} {{song.audioId}} {{song.videoId}}');
+            compile(element)(scope);
+            scope.$apply();
+
+            expect(element.html()).toBe('song_2 Parvathy Baul Sharath  scId videoId');
+        });
+
+    });
+});
