a5ae3d5b823db0741792f4dfa7c1f8ab9e24d41c
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-11-13 13:31:15 +0530

    fixing: popup was opening on hover

--- src/main/resources/assets/app/user/js/common/directives/thumbnail/couplet.js
+++ src/main/resources/assets/app/user/js/common/directives/thumbnail/couplet.js
@@ -14,6 +14,15 @@ thumbnailModule.directive("couplet", function() {
         },
         templateUrl:'/user/js/common/templates/thumbnail/couplet.html',
         controller:function($scope){
+            $scope.shouldShowDetails = false;
+
+            $scope.showDetails = function(){
+                $scope.shouldShowDetails = true;
+            }
+
+            $scope.hideDetails = function(){
+                $scope.shouldShowDetails = false;
+            }
         }
     }
 });
