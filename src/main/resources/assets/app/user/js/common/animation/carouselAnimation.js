2613a4d98300e3d70710e5fdcfc2c8c7ece1ae45
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-11-17 09:49:58 +0530

    added carousel animation

---
+++ src/main/resources/assets/app/user/js/common/animation/carouselAnimation.js
@@ -0,0 +1,35 @@
+var carouselAnimation = function () {
+    return {
+        beforeAddClass: function (element, className, done) {
+            var scope = element.scope();
+
+            if (className == 'ng-hide') {
+                var finishPoint = element.parent().width();
+                if(scope.direction !== 'right') {
+                    finishPoint = -finishPoint;
+                }
+                TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+            }
+            else {
+                done();
+            }
+        },
+        removeClass: function (element, className, done) {
+            var scope = element.scope();
+
+            if (className == 'ng-hide') {
+                element.removeClass('ng-hide');
+
+                var startPoint = element.parent().width();
+                if(scope.direction === 'right') {
+                    startPoint = -startPoint;
+                }
+
+                TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+            }
+            else {
+                done();
+            }
+        }
+    };
+};
--- src/main/resources/assets/app/user/js/songs/controllers/songDetailsController.js
+++ src/main/resources/assets/app/user/js/songs/controllers/songDetailsController.js
@@ -38,14 +38,6 @@ var songDetailsController = function($scope,$location,songsContentService){
         return false;
     }
 
-        $scope.slides = [
-            {image: 'http://www.pachd.com/free-images/abstract-images/abstract-04.jpg', description: 'Image 00'},
-            {image: 'http://www.pachd.com/free-images/abstract-images/abstract-04.jpg', description: 'Image 01'},
-            {image: 'http://www.pachd.com/free-images/abstract-images/abstract-04.jpg', description: 'Image 02'},
-            {image: 'http://www.pachd.com/free-images/abstract-images/abstract-04.jpg', description: 'Image 03'},
-            {image: 'http://www.pachd.com/free-images/abstract-images/abstract-04.jpg', description: 'Image 04'}
-        ];
-
         $scope.direction = 'left';
         $scope.currentIndex = 0;
 
@@ -73,38 +65,4 @@ var songDetailsController = function($scope,$location,songsContentService){
 
 songDetailsApp
 .controller('songDetailsController',['$scope','$location','songsContentService',songDetailsController])
-.animation('.slide-animation', function () {
-    return {
-        beforeAddClass: function (element, className, done) {
-            var scope = element.scope();
-
-            if (className == 'ng-hide') {
-                var finishPoint = element.parent().width();
-                if(scope.direction !== 'right') {
-                    finishPoint = -finishPoint;
-                }
-                TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-            }
-            else {
-                done();
-            }
-        },
-        removeClass: function (element, className, done) {
-            var scope = element.scope();
-
-            if (className == 'ng-hide') {
-                element.removeClass('ng-hide');
-
-                var startPoint = element.parent().width();
-                if(scope.direction === 'right') {
-                    startPoint = -startPoint;
-                }
-
-                TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
-            }
-            else {
-                done();
-            }
-        }
-    };
-});
+.animation('.slide-animation', carouselAnimation);
