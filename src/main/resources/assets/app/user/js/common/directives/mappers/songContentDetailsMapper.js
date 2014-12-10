1c0a9e5a5036cb46d4fbf17867d5bf8abbc34742
padma <padmavb@thoughtworks.com>
2014-12-10 12:18:58 +0530

    Added multiple singer functionality in lightbox and details page

--- src/main/resources/assets/app/user/js/common/directives/mappers/songContentDetailsMapper.js
+++ src/main/resources/assets/app/user/js/common/directives/mappers/songContentDetailsMapper.js
@@ -10,14 +10,22 @@ thumbnailModule.directive("songContentDetailsMapper", function(nameService) {
         },
         templateUrl:'/user/js/common/templates/mappers/songContentDetailsMapper.html',
         controller:function($scope){
+            var getSingers = function(singers){
+                var value = "" + singers[0].name;
+                for(var index=1;index< singers.length;index++){
+                   value += ' & '+singers[index].name;
+                }
+                return value;
+            };
+
             $scope.song = {
             "id":$scope.details.id,
             "contentId":"song_"+$scope.details.id,
-             "videoId":$scope.details.youtubeVideoId,
-             "audioUrl":$scope.details.soundCloudTrackID,
-             "singer":$scope.details.singers[0].name,
-             "poet":$scope.details.poets[0].name
-             };
+            "videoId":$scope.details.youtubeVideoId,
+            "audioUrl":$scope.details.soundCloudTrackID,
+            "singer":getSingers($scope.details.singers),
+            "poet":$scope.details.poets[0].name
+            };
         }
     }
 });
