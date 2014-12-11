b7a7b4be980cb459d5a2e0edb8bdb0d4f7fef1df
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-12-11 11:29:19 +0530

    added Umbrella title id

--- src/main/resources/migrations_manual/0004_SONG_TABLE_DATA_INSERTION.sql
+++ src/main/resources/migrations_manual/0004_SONG_TABLE_DATA_INSERTION.sql
@@ -6,6 +6,7 @@ INSERT INTO SONG(
     DURATION,
     YOUTUBE_VIDEO_ID,
     SONG_TITLE_ID,
+    UMBRELLA_TITLE_ID,
     SONG_CATEGORY,
     MEDIA_CATEGORY,
     SOUNDCLOUD_TRACK_ID,
@@ -13,6 +14,6 @@ INSERT INTO SONG(
     THUMBNAIL_URL
 )
 VALUES
-(TRUE,'5:45','tNh2kjmSzPw',1,1,4,'173752179',TRUE,'http://3.bp.blogspot.com/-kwpgiMcXc24/TcOcowo6mTI/AAAAAAAAA9w/uNt6ZsJadDg/s1600/parvathy_baul03.jpg'),
-(TRUE,'5:10','7Gg0vSOZhJQ',2,1,3,'173752179',TRUE,'http://i.ytimg.com/vi/J4IU5tDlD_s/mqdefault.jpg'),
-(TRUE,'7:05','J4IU5tDlD_s',3,1,4,'173752179',TRUE,'http://mountainshepherds.com/wp-content/gallery/archival/gauradevi.jpg');
+(TRUE,'5:45','tNh2kjmSzPw',1,4,1,4,'173752179',TRUE,'http://3.bp.blogspot.com/-kwpgiMcXc24/TcOcowo6mTI/AAAAAAAAA9w/uNt6ZsJadDg/s1600/parvathy_baul03.jpg'),
+(TRUE,'5:10','7Gg0vSOZhJQ',2,5,1,3,'173752179',TRUE,'http://i.ytimg.com/vi/J4IU5tDlD_s/mqdefault.jpg'),
+(TRUE,'7:05','J4IU5tDlD_s',3,6,1,4,'173752179',TRUE,'http://mountainshepherds.com/wp-content/gallery/archival/gauradevi.jpg');
