4047125863f11b5129f5b347b3263745245d6a62
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-12-18 11:59:00 +0530

    Array move implemented

--- src/main/resources/assets/app/admin/js/directives/lyrics.js
+++ src/main/resources/assets/app/admin/js/directives/lyrics.js
@@ -23,11 +23,23 @@ songsAdminApp.directive("lyrics", function() {
                 this.splice(index, 0, item);
             };
 
+            Array.prototype.move = function (old_index, new_index) {
+                if (new_index >= this.length) {
+                    var k = new_index - this.length;
+                    while ((k--) + 1) {
+                        this.push(undefined);
+                    }
+                }
+                this.splice(new_index, 0, this.splice(old_index, 1)[0]);
+                return this; // for testing purposes
+            };
+
             $scope.moveItemUp = function(){
                 var selectedSongIndex = $scope.lyricsTextList.indexOf($scope.selectedLyricsText);
-                var songToBeMovedUp = $scope.lyricsTextList.splice(selectedSongIndex, 1);
+                if(selectedSongIndex<0)
+                    return;
 
-                $scope.lyricsTextList.insert(selectedSongIndex - 1, songToBeMovedUp[0].englishTransliteration);
+                $scope.lyricsTextList.move(selectedSongIndex, selectedSongIndex-1);
             }
 
         }
