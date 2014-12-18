a98b220b5e3450ee89a7c99f33c65eb8a7ba83b5
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2014-12-18 12:04:37 +0530

    move down implemented in lyrics

--- src/main/resources/assets/app/admin/js/directives/lyrics.js
+++ src/main/resources/assets/app/admin/js/directives/lyrics.js
@@ -19,10 +19,6 @@ songsAdminApp.directive("lyrics", function() {
                 $scope.lyricsText = "";
             }
 
-            Array.prototype.insert = function (index, item) {
-                this.splice(index, 0, item);
-            };
-
             Array.prototype.move = function (old_index, new_index) {
                 if (new_index >= this.length) {
                     var k = new_index - this.length;
@@ -36,10 +32,18 @@ songsAdminApp.directive("lyrics", function() {
 
             $scope.moveItemUp = function(){
                 var selectedSongIndex = $scope.lyricsTextList.indexOf($scope.selectedLyricsText);
-                if(selectedSongIndex<0)
+                if(selectedSongIndex<=0)
                     return;
 
                 $scope.lyricsTextList.move(selectedSongIndex, selectedSongIndex-1);
+            }
+
+            $scope.moveItemDown = function(){
+                var selectedSongIndex = $scope.lyricsTextList.indexOf($scope.selectedLyricsText);
+                if(selectedSongIndex>=$scope.lyricsTextList.length-1)
+                    return;
+
+                $scope.lyricsTextList.move(selectedSongIndex, selectedSongIndex+1);
             }
 
         }
