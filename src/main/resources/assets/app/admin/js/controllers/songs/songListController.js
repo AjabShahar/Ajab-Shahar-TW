51a81d4769fb564c639e6e853509e7f8c9ba06e0
Jaideep Rana <jaideeprana45@yahoo.com>
2014-12-24 15:46:45 +0530

    Filter for is published on songs authoring page. <Jaideep>

--- src/main/resources/assets/app/admin/js/controllers/songs/songListController.js
+++ src/main/resources/assets/app/admin/js/controllers/songs/songListController.js
@@ -7,6 +7,7 @@ var songListController = function($scope, contentService){
                 var toBeAdded={};
                 toBeAdded.title = value.englishTransliterationTitle;
                 toBeAdded.categoryName = value.category;
+                toBeAdded.publish = value.publish;
                 toBeAdded.singerNames = _.reduce(value.singers, function(memo, value, index){ return ((index!=0)?' ,':'')+ memo+ value.name; },'');
                 toBeAdded.poetNames = _.reduce(value.poet, function(memo, value, index){ return ((index!=0)?' ,':'')+ memo+ value.name; },'');
                 toBeAdded.id = value.id;
