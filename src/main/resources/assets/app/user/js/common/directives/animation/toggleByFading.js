a621d3aa196b7c94ceb513c05511b42dd9596c34
avee9 <kauravee101@gmail.com>
2015-01-02 15:44:40 +0530

    Avi | Design fix for textarea on admin song details page

--- src/main/resources/assets/app/user/js/common/directives/animation/toggleByFading.js
+++ src/main/resources/assets/app/user/js/common/directives/animation/toggleByFading.js
@@ -2,7 +2,7 @@ animationModule.animation('.toggleByFading', function () {
   return {
     beforeAddClass: function (element, className, done) {
       if (className == 'ng-hide') {
-        element.hide();
+        element.fadeOut();
       }
       else {
              done();
