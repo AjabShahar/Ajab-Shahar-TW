7a7fa8368692f6bef697a35b898cdeaa2c1b19bb
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2015-01-06 10:53:12 +0530

    fixing tests

--- src/main/resources/assets/app/user/js/common/directives/animation/toggleBySlide.js
+++ src/main/resources/assets/app/user/js/common/directives/animation/toggleBySlide.js
@@ -2,7 +2,7 @@ animationModule.animation('.toggleBySlide', function () {
   return {
     beforeAddClass: function (element, className, done) {
       if (className == 'ng-hide') {
-        element.slideUp();
+        element.slideUp(1000);
       }
       else {
              done();
@@ -11,7 +11,7 @@ animationModule.animation('.toggleBySlide', function () {
       beforeRemoveClass: function (element, className, done) {
         if (className == 'ng-hide') {
             element.removeClass('ng-hide');
-            element.slideDown();
+            element.slideDown(1000);
           }
           else {
                  done();
