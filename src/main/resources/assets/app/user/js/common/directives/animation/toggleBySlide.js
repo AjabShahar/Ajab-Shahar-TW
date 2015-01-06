1c7b622a5d899c19083c831167f1e5b2014e90b9
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2015-01-06 15:17:26 +0530

    making toggle slide 750.

--- src/main/resources/assets/app/user/js/common/directives/animation/toggleBySlide.js
+++ src/main/resources/assets/app/user/js/common/directives/animation/toggleBySlide.js
@@ -2,7 +2,7 @@ animationModule.animation('.toggleBySlide', function () {
   return {
     beforeAddClass: function (element, className, done) {
       if (className == 'ng-hide') {
-        element.slideUp(1000);
+        element.slideUp(750);
       }
       else {
              done();
@@ -11,7 +11,7 @@ animationModule.animation('.toggleBySlide', function () {
       beforeRemoveClass: function (element, className, done) {
         if (className == 'ng-hide') {
             element.removeClass('ng-hide');
-            element.slideDown(1000);
+            element.slideDown(750);
           }
           else {
                  done();
