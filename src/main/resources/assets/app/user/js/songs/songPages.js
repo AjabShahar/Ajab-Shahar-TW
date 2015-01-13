7ceb67033e3cebbd3040f25f57db2a4738948920
avee9 <kauravee101@gmail.com>
2015-01-13 12:25:28 +0530

    Avi | Refactoring songs.less, gridview.less and common.less

--- src/main/resources/assets/app/user/js/songs/songPages.js
+++ src/main/resources/assets/app/user/js/songs/songPages.js
@@ -2,9 +2,9 @@
  	"use strict";
 
  	$(document).ready(function() {
- 		$(".filter").on("click", function() {
+ 		$(".filter-button").on("click", function() {
  			$(".nav-list").toggleClass("show");
- 			$(".ajab-wrapper").toggleClass("left-menu-open");
+
  			return false;
  		});
     }); // Close DOM ready.
