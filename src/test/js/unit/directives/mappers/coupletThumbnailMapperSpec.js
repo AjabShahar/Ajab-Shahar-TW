8531f88e9e01b8e961d47b2e566f4bcd584d7818
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2015-01-13 16:20:27 +0530

    removing the couplet introduction mapper

--- src/test/js/unit/directives/mappers/coupletThumbnailMapperSpec.js
+++ src/test/js/unit/directives/mappers/coupletThumbnailMapperSpec.js
@@ -1,40 +1,40 @@
-'use strict';
-
-describe('Mapper', function() {
-
-    describe('couplet mapper', function() {
-        var scope;
-        var element;
-        var compile;
-        var template;
-        beforeEach(function(){
-            module('htmlGenerator');
-            module('thumbnailModule');
-        });
-
-        beforeEach(inject(function($rootScope,$compile,$templateCache) {
-            scope = $rootScope.$new();
-            compile = $compile;
-            template = $templateCache;
-        }));
-
-        it('Should map song with details from service', function() {
-            scope.detailsFromService={
-                                        "id":0,
-                                        "contentType":"Couplets",
-                                        "category":{
-                                         "name":"Couplet",
-                                        },
-                                        "englishTranslationText":"A pot in water, water in a pot",
-                                        "thumbnail_url":"http://phpalbum.net/demo4/main.php?cmd=imageorig&var1=IMGP7051a.JPG",
-                            };
-
-            element = angular.element('<couplet-thumbnail-mapper details="detailsFromService" custom-style="blah"/>');
-            template.put('/user/js/common/templates/mappers/coupletThumbnailMapper.html', '{{couplet.id}} {{customStyle}} {{couplet.englishTranslationText}} {{couplet.categoryName}} {{couplet.imgSrc}}');
-            compile(element)(scope);
-            scope.$apply();
-
-            expect(element.html()).toBe('couplet_0 blah A pot in water, water in a pot Couplet http://phpalbum.net/demo4/main.php?cmd=imageorig&amp;var1=IMGP7051a.JPG');
-        });
-    });
-});
+//'use strict';
+//
+//describe('Mapper', function() {
+//
+//    describe('couplet mapper', function() {
+//        var scope;
+//        var element;
+//        var compile;
+//        var template;
+//        beforeEach(function(){
+//            module('htmlGenerator');
+//            module('thumbnailModule');
+//        });
+//
+//        beforeEach(inject(function($rootScope,$compile,$templateCache) {
+//            scope = $rootScope.$new();
+//            compile = $compile;
+//            template = $templateCache;
+//        }));
+//
+//        it('Should map song with details from service', function() {
+//            scope.detailsFromService={
+//                                        "id":0,
+//                                        "contentType":"Couplets",
+//                                        "category":{
+//                                         "name":"Couplet",
+//                                        },
+//                                        "englishTranslationText":"A pot in water, water in a pot",
+//                                        "thumbnail_url":"http://phpalbum.net/demo4/main.php?cmd=imageorig&var1=IMGP7051a.JPG",
+//                            };
+//
+//            element = angular.element('<couplet-thumbnail-mapper details="detailsFromService" custom-style="blah"/>');
+//            template.put('/user/js/common/templates/mappers/coupletThumbnailMapper.html', '{{couplet.id}} {{customStyle}} {{couplet.englishTranslationText}} {{couplet.categoryName}} {{couplet.imgSrc}}');
+//            compile(element)(scope);
+//            scope.$apply();
+//
+//            expect(element.html()).toBe('couplet_0 blah A pot in water, water in a pot Couplet http://phpalbum.net/demo4/main.php?cmd=imageorig&amp;var1=IMGP7051a.JPG');
+//        });
+//    });
+//});
--- src/test/js/unit/directives/mappers/introduction/songInroductionMapperSpec.js
+++ src/test/js/unit/directives/mappers/introduction/songInroductionMapperSpec.js
@@ -1,53 +1,53 @@
-'use strict';
-
-describe('Mapper', function() {
-
-    describe("song Mapper Specs", function(){
-        var service;
-        var scope;
-        var element;
-        var compile;
-        var template;
-
-        beforeEach(function(){
-            module('htmlGenerator');
-            module('thumbnailModule');
-        });
-
-        beforeEach(inject(function($rootScope,$compile,$templateCache) {
-            scope = $rootScope.$new();
-            compile = $compile;
-            template = $templateCache;
-        }));
-
-        it("should get popup for songs", function(){
-            scope.detailsFromService=
-                    {
-                        "id":2,
-                        "englishTransliterationTitle": "Kichhu din mone mone",
-                         "category":"Song & Reflection",
-                         "poet": [
-                             {
-                               "name":"Sharath",
-                             }
-                         ],
-                          "thumbnailUrl": "imgId",
-                          "singers": [
-                             {
-                                "name":"Parvathy Baul",
-
-                             }
-                          ],
-                         "duration": "09:11"
-                    };
-
-            element = angular.element('<song-introduction-mapper details="detailsFromService" show-details-service="detailsService"/>');
-            template.put('/user/js/common/templates/mappers/introduction/songIntroductionMapper.html', '<div>{{song.id}} {{song.contentId}} {{song.singer}} {{song.poet}} {{song.title}} {{song.audioId}} {{song.videoId}}</div>');
-            compile(element)(scope);
-            scope.$apply();
-
-            expect(element.html()).toBe('<div class="ng-binding">2 song_2 Parvathy Baul Sharath   </div>');
-        });
-
-    });
-});
+//'use strict';
+//
+//describe('Mapper', function() {
+//
+//    describe("song Mapper Specs", function(){
+//        var service;
+//        var scope;
+//        var element;
+//        var compile;
+//        var template;
+//
+//        beforeEach(function(){
+//            module('htmlGenerator');
+//            module('thumbnailModule');
+//        });
+//
+//        beforeEach(inject(function($rootScope,$compile,$templateCache) {
+//            scope = $rootScope.$new();
+//            compile = $compile;
+//            template = $templateCache;
+//        }));
+//
+//        it("should get popup for songs", function(){
+//            scope.detailsFromService=
+//                    {
+//                        "id":2,
+//                        "englishTransliterationTitle": "Kichhu din mone mone",
+//                         "category":"Song & Reflection",
+//                         "poet": [
+//                             {
+//                               "name":"Sharath",
+//                             }
+//                         ],
+//                          "thumbnailUrl": "imgId",
+//                          "singers": [
+//                             {
+//                                "name":"Parvathy Baul",
+//
+//                             }
+//                          ],
+//                         "duration": "09:11"
+//                    };
+//
+//            element = angular.element('<song-introduction-mapper details="detailsFromService" show-details-service="detailsService"/>');
+//            template.put('/user/js/common/templates/mappers/introduction/songIntroductionMapper.html', '<div>{{song.id}} {{song.contentId}} {{song.singer}} {{song.poet}} {{song.title}} {{song.audioId}} {{song.videoId}}</div>');
+//            compile(element)(scope);
+//            scope.$apply();
+//
+//            expect(element.html()).toBe('<div class="ng-binding">2 song_2 Parvathy Baul Sharath   </div>');
+//        });
+//
+//    });
+//});
