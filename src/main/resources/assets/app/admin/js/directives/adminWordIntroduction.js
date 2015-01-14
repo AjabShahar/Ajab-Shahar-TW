7f60b6ff9d0e28bcc087b650078578ec56499441
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2015-01-14 10:43:18 +0530

    fixing the word introduction

--- src/main/resources/assets/app/admin/js/directives/adminWordIntroduction.js
+++ src/main/resources/assets/app/admin/js/directives/adminWordIntroduction.js
@@ -11,14 +11,13 @@ wordsAdminApp.directive("adminWordIntroduction", function() {
         templateUrl:'/admin/js/templates/adminWordIntroduction.html',
         controller:function($scope){
             $scope.addToWordIntroduction = function(){
-                var wordIntroduction = {};
-                  var wordIntroductions = $scope.wordIntroduction.split(/\n{2,}/g);
-                  for(var i=0;i<wordIntroductions.length;){
-                     var wordIntroduction = {};
-                     wordIntroduction.introduction_text = wordIntroductions[i];
-                     $scope.wordIntroductions[i] = wordIntroduction;
-                     i=i+1;
-                  }
+                var wordIntroductions = $scope.wordIntroduction.split(/\n{2,}/g);
+                for(var i=0;i<wordIntroductions.length;){
+                    var wordIntroduction = {};
+                    wordIntroduction.introduction_text = wordIntroductions[i];
+                    $scope.wordIntroductions[i] = wordIntroduction;
+                    i=i+1;
+                }
             };
         }
     }
