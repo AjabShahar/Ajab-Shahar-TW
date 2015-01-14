404b42415bf1fd826a8c6c0583bedcfcec079be3
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-14 19:09:34 +0530

    WIP for words. <Jaideep>

---
+++ src/main/resources/assets/app/user/js/EditorsChoice/controllers/wordsEditorsChoiceController.js
@@ -0,0 +1,23 @@
+var wordsEditorsChoiceController = function($scope,contentService, $location, $window){
+	$scope.init = function(){
+		$scope.url = $location.absUrl();
+
+        contentService.getWordsLandingPageContent().then(function(result){
+            $scope.words = result.data;
+        });
+	}
+
+	$scope.setPageHeight = function(){
+        var homepageContent = jQuery('.' + 'homepage-content')[0];
+        var pxLayers = jQuery('.' + 'pxLayers');
+
+        _.each(pxLayers, function(pxLayer){
+            pxLayer.style.height = (homepageContent.offsetHeight+400)+"px";
+        });
+        homepageContent.style.height = (homepageContent.offsetHeight+300)+"px";
+    }
+
+	$scope.init();
+}
+
+editorsChoiceApp.controller('songsEditorsChoiceController',['$scope','contentService','$location', '$window',wordsEditorsChoiceController]);
