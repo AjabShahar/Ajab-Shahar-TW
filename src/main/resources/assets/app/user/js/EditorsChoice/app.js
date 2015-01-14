ccdc877ac54ed6e2f2a83daa6666a7cb4418c686
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-14 13:54:41 +0530

    Removing angular-parallax and its usage as its not required. <Jaideep>

--- src/main/resources/assets/app/user/js/EditorsChoice/app.js
+++ src/main/resources/assets/app/user/js/EditorsChoice/app.js
@@ -1,4 +1,4 @@
-var editorsChoiceApp = angular.module('editorsChoiceApp',['angular-parallax','thumbnailModule','mediaPlayer','popupSupport','htmlGenerator','headerModule','djds4rce.angular-socialshare']);
+var editorsChoiceApp = angular.module('editorsChoiceApp',['thumbnailModule','mediaPlayer','popupSupport','htmlGenerator','headerModule','djds4rce.angular-socialshare']);
 
 editorsChoiceApp.config(resourceUrlWhiteList);
 editorsChoiceApp.factory('contentService', ['$http', contentService]);
