9bf7ef3177e164d2fd413020ee43a5eb54f1776f
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-14 17:28:31 +0530

    WIP for words editors choice(currently in broken state). <Jaideep>

--- src/main/resources/assets/app/user/js/EditorsChoice/services/contentService.js
+++ src/main/resources/assets/app/user/js/EditorsChoice/services/contentService.js
@@ -7,8 +7,14 @@ var contentService = function ($http) {
     return $http.get('/api/songs/getPublishedSongs?randomSongsEnabled=true');
   };
 
+  var getWordsLandingPageContent = function () {
+    // words/getPublishedSongs
+    return $http.get('/api/words');
+  };
+
   return {
     getMainLandingPageThumbnails: getMainLandingPageThumbnails,
     getSongsLandingPageThumbnails: getSongsLandingPageThumbnails,
+    getWordsLandingPageContent: getWordsLandingPageContent,
   };
 };
---
+++ src/main/resources/assets/app/user/js/common/directives/words/wordBubble.js
@@ -0,0 +1,16 @@
+'use strict';
+
+thumbnailModule.directive("wordBubble", function() {
+    return {
+        restrict: 'E',
+        scope: {
+            id:'@',
+            wordTransliteration:'@',
+            wordTranslation:'@',
+            transliterationIntro:'@',
+        },
+        templateUrl:'/user/js/common/templates/words/wordBubble.html',
+        controller: function($scope) {
+        }
+    }
+});
