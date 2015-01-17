bbb224850914e2f2d8cf624e02bac5099dc577fc
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-17 14:29:00 +0530

    WIP for words. <Jaideep>

--- src/main/resources/assets/app/user/js/words/controllers/wordsEditorsChoiceController.js
+++ src/main/resources/assets/app/user/js/words/controllers/wordsEditorsChoiceController.js
@@ -5,6 +5,16 @@ var wordsEditorsChoiceController = function($scope,contentService, $location, $w
         });
 	}
 
+	$scope.setPageHeight = function(){
+    var homepageContent = jQuery('.' + 'homepage-content')[0];
+    var pxLayers = jQuery('.' + 'pxLayers');
+
+    _.each(pxLayers, function(pxLayer){
+        pxLayer.style.height = (homepageContent.offsetHeight+400)+"px";
+    });
+    homepageContent.style.height = (homepageContent.offsetHeight+300)+"px";
+    }
+
 	$scope.init();
 }
 
