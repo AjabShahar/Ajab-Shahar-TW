47fdf47b35526c5bb13577d3966cea657b0e10f0
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-23 12:54:14 +0530

    Fixing issue with the shell file. <Jaideep>

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,2 @@
-if [ -f .bash_profile ]; then
-cat as_pid_file | xargs kill -9
-else
+pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9
 java -jar platform-1.0-SNAPSHOT.jar server $1 > server.log &
-echo $! > as_pid_file
-fi
