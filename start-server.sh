db2b569184cf9bd0e132f7229e5e3266a004ea3b
Jaideep Rana <jaideeprana45@yahoo.com>
2015-01-23 14:59:28 +0530

    Minor correction. <Jaideep>

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9;
-java -jar target/platform-1.0-SNAPSHOT.jar server $1 > server.log &
+java -jar platform-1.0-SNAPSHOT.jar server $1 > server.log &
