fef16be5e821ff8305f4820f5096294b92b95756
padma <padmavb@thoughtworks.com>
2015-01-30 17:49:08 +0530

    Added feedback, word intro (hindi, english) are not mandatory fields on word details admin page

--- src/main/resources/assets/app/admin/js/directives/adminWordIntroduction.js
+++ src/main/resources/assets/app/admin/js/directives/adminWordIntroduction.js
@@ -10,13 +10,13 @@ wordsAdminApp.directive("adminWordIntroduction", function() {
         templateUrl:'/admin-js/templates/adminWordIntroduction.html',
         controller:function($scope){
             $scope.addToWordIntroduction = function(){
-                var introTextOriginals = $scope.wordIntroHindi.split(/\n{2,}/g);
-                var introTextTranslations = $scope.wordIntroEnglish.split(/\n{2,}/g);
-                var total =  introTextOriginals.length>0 ? introTextOriginals.length : introTextTranslations.length;
+                var introTextOriginals = $scope.wordIntroHindi !=null ? $scope.wordIntroHindi.split(/\n{2,}/g) : [];
+                var introTextTranslations = $scope.wordIntroEnglish !=null ? $scope.wordIntroEnglish.split(/\n{2,}/g) : [];
+                var total =  introTextOriginals.length > 0 ? introTextOriginals.length : introTextTranslations.length;
                 for(var i=0; i < total; i++){
                     var wordIntroduction = {};
-                    wordIntroduction.wordIntroHindi ="<p>" + introTextOriginals[i].split("\n").join("<br />") +"</p>";
-                    wordIntroduction.wordIntroEnglish ="<p>" + introTextTranslations[i].split("\n").join("<br />") +"</p>";
+                    wordIntroduction.wordIntroHindi = introTextOriginals.length >0 ? "<p>" + introTextOriginals[i].split("\n").join("<br />") +"</p>" : "";
+                    wordIntroduction.wordIntroEnglish = introTextTranslations.length >0 ?"<p>" + introTextTranslations[i].split("\n").join("<br />") +"</p>" : "";
                     $scope.wordIntroductions.push(wordIntroduction)
                 }
                 $scope.wordIntroduction = "";
