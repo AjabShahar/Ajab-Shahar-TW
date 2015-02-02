d00421d352e92be1d393e8ef7156d37ec6f0ec3e
padma <padmavb@thoughtworks.com>
2015-02-02 16:13:42 +0530

    implemented feedback, limiting number of characters on word thumbnail

--- src/main/resources/assets/app/user/js/common/directives/words/wordOverview.js
+++ src/main/resources/assets/app/user/js/common/directives/words/wordOverview.js
@@ -12,7 +12,7 @@ thumbnailModule.directive("wordOverview", function() {
         },
         templateUrl:'/user-js/common/templates/words/wordOverview.html',
         controller: function($scope) {
-           $scope.people = $scope.writers[0].name;
+           $scope.people = $scope.writers.length >0 ? $scope.writers[0].name : "";
            for(var i=1;i<$scope.writers.length;i++){
               $scope.people += " & " +$scope.writers[i].name;
            }
