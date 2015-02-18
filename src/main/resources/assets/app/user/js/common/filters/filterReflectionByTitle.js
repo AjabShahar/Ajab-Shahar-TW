5c2aa20e3bbdc15a2584757f3b42d701c0520cee
unknown <sswaroop@INsoumya-pc.corporate.thoughtworks.com>
2015-02-18 11:46:12 +0530

    reflections grid page

--- src/main/resources/assets/app/user/js/common/filters/filterReflectionByTitle.js
+++ src/main/resources/assets/app/user/js/common/filters/filterReflectionByTitle.js
@@ -6,14 +6,14 @@ var filterReflectionByTitle = filterModule.filter('filterReflectionByTitle', fun
         }
 
         if(filterCriteria==null || filterCriteria==='')
-            return songs;
+            return reflections;
 
         var filtered = _.reduce(reflections, function(filteredList,reflection){
             var title = (filterCriteria.contentTextRepresentation === 'Transliteration')?
                 reflection.title :
                 reflection.title;
             if(strStartsWith(title.toUpperCase(),filterCriteria.alphabet.toUpperCase()))
-                filteredList.push(song);
+                filteredList.push(reflection);
 
             return filteredList;
         },[]);
