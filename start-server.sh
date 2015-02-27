fbe38694c914795ea842dad6ff854ef9f59cdba3
Vinod R <Vinod.Ramakrishnan@target.com>
2015-02-27 14:58:13 +0530

    Removing the parameter for start server <Vinod/Jaideep>

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9
-nohup java -jar platform-1.0-SNAPSHOT.jar server $1 > server.log &
+nohup java -jar platform-1.0-SNAPSHOT.jar server appserver.yml > server.log &
