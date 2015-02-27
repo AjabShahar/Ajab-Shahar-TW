7c077a44cb44a31fd2e9ea88fd0c184541aa68b0
Vinod R <Vinod.Ramakrishnan@target.com>
2015-02-27 15:22:56 +0530

    Splitting start/stop scripts, adding log file configuration <Vinod/Jaideep>

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9
-nohup java -jar platform-1.0-SNAPSHOT.jar server appserver.yml > server.log &
+nohup java -jar platform-1.0-SNAPSHOT.jar server appserver.yml > /dev/null &
---
+++ stop-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9
