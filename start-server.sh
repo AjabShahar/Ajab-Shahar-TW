f3cc2adb494614c12e5beb550b0fad5326264956
Jaideep Rana <jaideeprana45@yahoo.com>
2015-02-27 11:53:00 +0530

    Removing disown from startup script <Vinod/Jaideep>

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,3 @@
-pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9 ;
+#!/bin/sh
+pgrep -f platform-1.0-SNAPSHOT.jar | xargs kill -9
 nohup java -jar platform-1.0-SNAPSHOT.jar server $1 > server.log &
-disown
