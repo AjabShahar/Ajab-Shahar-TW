73eccca6b05a4288bbe9b7efbc6e0a54c9c54e60
Avinash Kaur <avikaur@thoughtworks.com>
2015-03-03 12:58:53 +0530

    #51 adding atyles for custom scrollbar for word lightbox and deleting extra plugin files <Avi>

--- src/main/resources/assets/app/user/js/common/directives/customScroller/ajabShaharScrollBar.js
+++ src/main/resources/assets/app/user/js/common/directives/customScroller/ajabShaharScrollBar.js
@@ -1,28 +1,8 @@
-//ajabShaharScrollBar.directive("customScrollBar", function () {
-//    return {
-//        link : function(scope,element,attrs){
-//            $(element).jScrollPane({
-//                width:'600px',
-//                height:'500px',
-//                background:'#FF0000'
-//            });
-//        }
-//    }
-//     Ps.initialize(document.getElementById('av'));
-//});
-//angular.module("ajabShaharScrollBar").controller("MyCtrl", function ($scope) {
-//    $scope.paneConfig = {
-//        verticalDragMinHeight: 100
-//    }
-//});
 
 ajabShaharScrollBar.directive('customScrollBar',function(){
     return {
         restrict:'AE',
-//        transclude: true,
-//        template: "<div class='scroll-pane' ng-transclude></div>",
         link: function (scope, elm, attrs) {
-//                console.log(elm);
             Ps.initialize(document.getElementById('scrollPaneContainer'));
         }
     };
