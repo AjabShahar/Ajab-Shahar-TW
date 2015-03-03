633bc0537a2cc154a2a6bc7b1717d3d9af205409
Avinash Kaur <avikaur@thoughtworks.com>
2015-03-03 12:17:23 +0530

    #51 adding perfect scroller plugin <Avi/Indraneel>

--- src/main/resources/assets/app/user/js/common/directives/customScroller/ajabShaharScrollBar.js
+++ src/main/resources/assets/app/user/js/common/directives/customScroller/ajabShaharScrollBar.js
@@ -1,11 +1,29 @@
-ajabShaharScrollBar.directive("customScrollBar", function () {
+//ajabShaharScrollBar.directive("customScrollBar", function () {
+//    return {
+//        link : function(scope,element,attrs){
+//            $(element).jScrollPane({
+//                width:'600px',
+//                height:'500px',
+//                background:'#FF0000'
+//            });
+//        }
+//    }
+//     Ps.initialize(document.getElementById('av'));
+//});
+//angular.module("ajabShaharScrollBar").controller("MyCtrl", function ($scope) {
+//    $scope.paneConfig = {
+//        verticalDragMinHeight: 100
+//    }
+//});
+
+ajabShaharScrollBar.directive('customScrollBar',function(){
     return {
-        link : function(scope,element,attrs){
-            $(element).jScrollPane({
-                width:'600px',
-                height:'500px',
-                background:'#FF0000'
-            });
+        restrict:'AE',
+//        transclude: true,
+//        template: "<div class='scroll-pane' ng-transclude></div>",
+        link: function (scope, elm, attrs) {
+//                console.log(elm);
+            Ps.initialize(document.getElementById('scrollPaneContainer'));
         }
-    }
+    };
 });
