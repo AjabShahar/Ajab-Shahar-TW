3aa72594e7746f81c91f0a8470c52a1cab08d227
Claxton Correya <claxton@gmail.com>
2015-03-03 16:47:40 +0530

    Claxton - Adding header to glossary page

--- src/main/resources/assets/app/user/js/featuredContent/glossaryApp.js
+++ src/main/resources/assets/app/user/js/featuredContent/glossaryApp.js
@@ -1,3 +1,3 @@
-var glossaryApp = angular.module('glossaryApp', [])
+var glossaryApp = angular.module('glossaryApp', ['headerModule'])
 
 glossaryApp.factory('contentService', ['$http', contentService]);
