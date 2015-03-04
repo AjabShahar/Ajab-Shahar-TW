40320a0c4a77eeb70935d1a27f117b9419bd8b2a
Nandhakumar Ramanathan <nandhar@thoughtworks.com>
2015-03-04 19:56:27 +0530

    DEVOPS | Add sass watch task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,16 @@ var gulp = require("gulp"),
 var userModule = "./src/main/resources/assets/app/user";
 
 
+
+// SASS Related tasks
+
 gulp.task("sass:compile", function() {
 
   var outputStyle = "expanded"; // "nested" | "compact" | "compressed" | "expanded"
 
   return gulp.src(
-  					path.join(userModule, "/sass/ajabShahar.scss"
-  				))
+  					path.join(userModule, "/sass/ajabShahar.scss")
+  				)
 					.pipe(sourcemaps.init())
 					.pipe(sass({outputStyle: outputStyle}))
 					.pipe(sourcemaps.write())
@@ -22,5 +25,19 @@ gulp.task("sass:compile", function() {
 					));
 });
 
+gulp.task("sass:watch", function() {
+
+ 	gulp.watch(
+ 		path.join(userModule, "/sass/ajabShahar.scss"),
+ 		["sass:compile"]
+ 	)
+
+ 	// gutil.log("[watch:sass]", gutil.colors.yellow("Watching SASS files..."));
+
+});
+
+// Common tasks
 
 gulp.task("default", ["sass:compile"]);
+
+gulp.task("watch", ["sass:watch"]);
