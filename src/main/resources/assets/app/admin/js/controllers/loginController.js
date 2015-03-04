618c89a3e89858421450c1c0d98e0619960ababb
padma <padmavb@thoughtworks.com>
2015-03-04 11:41:12 +0530

    #17 | setting cookies on signin | Padma,Indraneel

--- src/main/resources/assets/app/admin/js/controllers/loginController.js
+++ src/main/resources/assets/app/admin/js/controllers/loginController.js
@@ -1,16 +1,17 @@
-var loginController = function($scope, $http,$window) {
+var loginController = adminApp.controller ('loginController',['$scope', '$http','$window','$cookies', function($scope, $http,$window,$cookies) {
 
     $scope.formInfo = {};
     $scope.login = function(){
 
         $http.post('/api/login',$scope.formInfo).success(function(args){
           $window.location.href = '/admin/home.html';
+          $cookies.user = "admin";
         }).
         error(function(args){
            alert("error" + args);
         });
     };
-};
+}]);
 
-adminApp.controller ('loginController',['$scope', '$http','$window',loginController]);
+
 
