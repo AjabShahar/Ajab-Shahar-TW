39e2177f34c8d9df73665108827d3b5603fde6fb
indraneelr <indraneel.rr@gmail.com>
2015-03-05 19:41:34 +0530

    #17 | fixing minor issues | indraneel

--- src/main/resources/assets/app/admin/js/controllers/loginController.js
+++ src/main/resources/assets/app/admin/js/controllers/loginController.js
@@ -1,11 +1,11 @@
-var loginController = adminApp.controller ('loginController',['$scope', '$http','$window', function($scope, $http,$window) {
+var loginController = adminApp.controller ('loginController',['$scope', '$http','$window','$cookies', function($scope, $http,$window,$cookies) {
 
     $scope.formInfo = {};
     $scope.login = function(){
 
         $http.post('/api/login',$scope.formInfo).success(function(args){
-            $window.location.href = '/admin/home.html';
             $cookies.authSessionId = $cookies.JSESSIONID;
+            $window.location.href = '/admin/home.html';
         }).
         error(function(args){
            alert("error" + args);
