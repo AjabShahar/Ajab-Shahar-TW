eb531cbaecc58d5cac2f04656f8beb812b9e6431
indraner <indraner@INindraner.local>
2015-03-30 11:32:02 +0530

    #45| renaming the generic filter utility | indraneel

--- web/app/user/js/common/models/sieve.js
+++ web/app/user/js/common/models/sieve.js
@@ -1,6 +1,6 @@
 var Ajabshahar = Ajabshahar || {};
 Ajabshahar.user = Ajabshahar.user || {};
-Ajabshahar.user.FilterModel = function (filterCriteria) {
+Ajabshahar.user.Sieve = function (filterCriteria) {
     var self = {};
     self.selectedFilters = [];
 
@@ -19,15 +19,14 @@ Ajabshahar.user.FilterModel = function (filterCriteria) {
     };
 
     var matches = function (item, path, value) {
-        if(value === undefined) return true;
+        if(value === undefined || value === "")  return true;
+
         var pathIndexes = path.split(".");
         var pathIndex = pathIndexes[0];
         if (endsWith(pathIndex, '[]')) {                       // is array
             pathIndex = substringUpto(pathIndex, '[]');
             if (_.isArray(item[pathIndex])) {
-
                 return item[pathIndex].some(function (child) {
-                    //console.log("endswith",item[pathIndex],path,substringAfter(path, '.'));
                     return matches(child, substringAfter(path, '.'), value);
                 })
             }
@@ -38,23 +37,37 @@ Ajabshahar.user.FilterModel = function (filterCriteria) {
                 return matches(item[pathIndexes[0]], substringAfter(path, '.'), value);
             }
         }
-
         return path === "" ? item === value : item[path] === value;
     };
 
     self.filter = function (items) {
         return items.filter(function (item) {
             return self.filterCriteria.every(function (criteria) {
-                return  matches(item, criteria.name, criteria.value);
+                var result = matches(item, criteria.name, criteria.value);
+                //console.log("filtermatches : ",result," | ",criteria.name ," | ",criteria.value," | ",item.englishTransliteration);
+                return  result;
             })
         })
 
     };
+
     self.clearFilters = function () {
         self.filterCriteria.forEach(function (criteria) {
             criteria["value"] = undefined;
         });
     };
+
+    self.setFilterCriteria = function(name,value){
+        var filterCriteria =_.find(self.filterCriteria,function(criteria){
+            return criteria.name === name;
+        });
+        filterCriteria.value=value;
+    };
+
+    self.removeFilterCriteria = function(filterName){
+        self.setFilterCriteria(filterName,undefined);
+    };
+
 
     return self;
 
--- web/test/config/karma.conf.js
+++ web/test/config/karma.conf.js
@@ -1,66 +1,67 @@
 'use strict';
 module.exports = function (config) {
-  config.set({
-    basePath: '../../app',
-    files: [
-      'common/lib/jquery/dist/jquery.min.js',
-      'common/lib/underscore/underscore-min.js',
-      'common/lib/angular/angular.js',
-      'common/lib/angular-animate/angular-animate.js',
-      'common/lib/angular-route/angular-route.js',
-      'common/lib/angular-mocks/angular-mocks.js',
-      'common/lib/angular-multi-select/angular-multi-select.js',
-      'common/lib/textAngular/dist/textAngular-sanitize.min.js',
-      'common/lib/textAngular/dist/textAngular.min.js',
-      'admin/js/services/**/*.js',
-      'admin/js/person/**/*.js',
-      'admin/js/words/**/*.js',
-      'admin/js/reflections/**/*.js',
-      'admin/js/gatherings/**/*.js',
-      'admin/js/**/*.js',
-      'user/js/common/modules/headerModule.js',
-      'user/js/common/directives/animation/animationModule.js',
-      'user/js/common/modules/thumbnailModule.js',
-      'user/js/common/modules/PopupSupport.js',
-      'user/js/common/modules/mediaPlayer.js',
-      'user/js/common/modules/filterModule.js',
-      'user/js/common/modules/lazyLoadModule.js',
-      'user/js/common/**/*.js',
-      'user/js/introduction/**/*.js',
-      'user/js/featuredContent/**/*.js',
-      'user/js/songs/services/songsContentService.js',
-      'user/js/songs/songDetailsApp.js',
-      'user/js/songs/songsApp.js',
-      'user/js/words/services/*.js',
-      'user/js/words/*.js',
-      'user/js/words/**/*.js',
-      'user/js/songs/**/*.js',
-      'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-cookies.js',
-      '../test/unit/**/*.js'
-    ],
-    autoWatch: true,
-    frameworks: ['jasmine'],
-    browsers: ['PhantomJS'],
-    reporters: ['dots', 'junit', 'coverage'],
-    preprocessors: {
-      '**/*.js': ['coverage']
-    },
-    logLevel: config.LOG_INFO,
-    singleRun: true,
-    junitReporter: {
-      outputFile: './test-results.xml'
-    },
-    coverageReporter: {
-      threshold: 85,
-      reporters: [
-        {
-          type: 'html',
-          dir: '../coverage/'
+    config.set({
+        basePath: '../../app',
+        files: [
+            'common/lib/jquery/dist/jquery.min.js',
+            'common/lib/underscore/underscore-min.js',
+            'common/lib/angular/angular.js',
+            'common/lib/angular-animate/angular-animate.js',
+            'common/lib/angular-route/angular-route.js',
+            'common/lib/angular-mocks/angular-mocks.js',
+            'common/lib/angular-multi-select/angular-multi-select.js',
+            'common/lib/textAngular/dist/textAngular-sanitize.min.js',
+            'common/lib/textAngular/dist/textAngular.min.js',
+            'common/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js',
+            'admin/js/services/**/*.js',
+            'admin/js/person/**/*.js',
+            'admin/js/words/**/*.js',
+            'admin/js/reflections/**/*.js',
+            'admin/js/gatherings/**/*.js',
+            'admin/js/**/*.js',
+            'user/js/common/modules/headerModule.js',
+            'user/js/common/directives/animation/animationModule.js',
+            'user/js/common/modules/thumbnailModule.js',
+            'user/js/common/modules/PopupSupport.js',
+            'user/js/common/modules/mediaPlayer.js',
+            'user/js/common/modules/filterModule.js',
+            'user/js/common/modules/lazyLoadModule.js',
+            'user/js/common/**/*.js',
+            'user/js/introduction/**/*.js',
+            'user/js/featuredContent/**/*.js',
+            'user/js/songs/services/songsContentService.js',
+            'user/js/songs/songDetailsApp.js',
+            'user/js/songs/songsApp.js',
+            'user/js/words/services/*.js',
+            'user/js/words/*.js',
+            'user/js/words/**/*.js',
+            'user/js/songs/**/*.js',
+            'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-cookies.js',
+            '../test/unit/**/*.js'
+        ],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['PhantomJS'],
+        reporters: ['dots', 'junit', 'coverage'],
+        preprocessors: {
+            '**/*.js': ['coverage']
         },
-        {
-          type: 'text-summary'
+        logLevel: config.LOG_INFO,
+        singleRun: true,
+        junitReporter: {
+            outputFile: './test-results.xml'
+        },
+        coverageReporter: {
+            threshold: 85,
+            reporters: [
+                {
+                    type: 'html',
+                    dir: '../coverage/'
+                },
+                {
+                    type: 'text-summary'
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
 };
