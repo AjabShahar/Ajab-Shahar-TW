22008eac004735f375a88144c792c8510913e59a
indraner <indraner@INindraner.local>
2015-03-30 11:33:27 +0530

    #45 | created new directives for the filter pardas 1,2 | indraneel

---
+++ web/app/user/js/common/directives/songs/songFilterCategory.js
@@ -0,0 +1,33 @@
+'use strict';
+
+filterModule.directive("songFilterCategory", function() {
+
+    return {
+        restrict:'EA',
+        templateUrl:'/user/js/common/templates/songs/songFilterCategory.html',
+        scope:{
+            criteriaList:"=",
+            selectHandler:"=",
+            deselectHandler:"=",
+            clearFilters:"="
+        },
+
+        link:function(scope,element,attrs){
+            scope.criteriaClicked = function(criteria){
+                //console.log("criteria clicked : ",criteria)
+                scope.selectHandler(criteria);
+            };
+
+            scope.criteriaRemoved = function(criteria){
+                //console.log("criteria removed : ",criteria)
+                scope.deselectHandler(criteria);
+            };
+
+            scope.resetFilters = function(){
+                //console.log("reset clicked")
+
+                scope.clearFilters();
+            }
+        }
+    }
+});
---
+++ web/app/user/js/common/directives/songs/songFilterParda.js
@@ -0,0 +1,19 @@
+'use strict';
+
+filterModule.directive("songFilterParda", function() {
+
+    return {
+        restrict:'EA',
+        templateUrl:'/user/js/common/templates/songs/songFilterParda.html',
+        scope:{
+            filterItems:"=",
+            selectHandler:"="
+        },
+
+        link:function(scope,element,attrs){
+            scope.itemClicked = function(item){
+                scope.selectHandler(item[0]);
+            };
+        }
+    }
+});
