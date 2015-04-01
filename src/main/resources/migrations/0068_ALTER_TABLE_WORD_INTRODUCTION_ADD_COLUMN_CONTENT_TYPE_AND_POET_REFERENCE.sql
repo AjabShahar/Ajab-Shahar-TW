c356b2492424f85f8fadead6e04422d511bf1804
Jaideep Rana <jaideeprana45@yahoo.com>
2015-04-01 13:06:01 +0530

    Fixing the syntax error in migration. <Jaideep>

--- src/main/resources/migrations/0068_ALTER_TABLE_WORD_INTRODUCTION_ADD_COLUMN_CONTENT_TYPE_AND_POET_REFERENCE.sql
+++ src/main/resources/migrations/0068_ALTER_TABLE_WORD_INTRODUCTION_ADD_COLUMN_CONTENT_TYPE_AND_POET_REFERENCE.sql
@@ -3,6 +3,5 @@
 --changeset chanakya_jaideep:68
 
 ALTER TABLE word_introduction ADD COLUMN content_type varchar(200);
-ALTER TABLE word_introduction ALTER COLUMN content_type;
 ALTER TABLE word_introduction ADD COLUMN poet_id integer;
 ALTER TABLE word_introduction ADD CONSTRAINT poet_id_fkey FOREIGN KEY (poet_id) REFERENCES person (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION;
