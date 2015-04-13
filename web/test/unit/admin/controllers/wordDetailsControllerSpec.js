0849ead1b0fd8900dd377baa87d7f3172ae86076
indraner <indraner@INindraner.local>
2015-04-13 18:14:19 +0530

    fixing frontend binding of reflections in word | padma, indraneel

--- web/test/unit/admin/controllers/wordDetailsControllerSpec.js
+++ web/test/unit/admin/controllers/wordDetailsControllerSpec.js
@@ -16,6 +16,8 @@ describe("Word details controller spec:", function () {
         $httpBackend = _$httpBackend_;
         $cookies.user = "admin";
 
+        spyOn($location, 'search').andReturn({id:1});
+
         _$controller_('wordDetailsController', {
             $scope: scope,
             $window: fakeWindow,
@@ -24,6 +26,8 @@ describe("Word details controller spec:", function () {
             PAGES: PAGES,
             loginVerifyService: loginVerifyService
         });
+
+
     }));
 
     beforeEach(function () {
@@ -32,11 +36,15 @@ describe("Word details controller spec:", function () {
     });
 
     describe("When initializing a word", function () {
+
         it("then should have people and writers", function () {
+            $httpBackend.expectGET("/api/words/edit?id=1").respond(test_word);
+
+
             $httpBackend.when("GET", "/api/people?role=Poet").respond({"people": "somePerson"});
             $httpBackend.when("GET", "/api/people").respond({"people": "somePerson"});
             $httpBackend.when("GET", "/api/category/word").respond(null);
-            $httpBackend.when("GET", "/api/reflections/summary").respond(null);
+            $httpBackend.when("GET", "/api/reflections/summary").respond(test_reflection_summaries);
             $httpBackend.when("GET", "/api/songs/getAllSongs").respond({
                 "songs": [{
                     "englishTransliterationTitle": "some title",
@@ -50,6 +58,30 @@ describe("Word details controller spec:", function () {
 
             expect(scope.writers).toBe("somePerson");
             expect(scope.people).toBe("somePerson");
+        });
+
+
+        it("it should display the linked reflections", function () {
+            $httpBackend.expectGET("/api/words/edit?id=1").respond(test_word);
+
+            $httpBackend.when("GET", "/api/people?role=Poet").respond({"people": "somePerson"});
+            $httpBackend.when("GET", "/api/people").respond({"people": "somePerson"});
+            $httpBackend.when("GET", "/api/category/word").respond(null);
+            $httpBackend.when("GET", "/api/reflections/summary").respond(test_reflection_summaries);
+            $httpBackend.when("GET", "/api/songs/getAllSongs").respond({
+                "songs": [{
+                    "englishTransliterationTitle": "some title",
+                    "singers": [],
+                    "words": {"words": []}
+                }]
+            });
+
+            scope.init();
+            $httpBackend.flush();
+
+            expect(scope.reflections[0].ticked).toBeFalsy();
+            expect(scope.reflections[1].ticked).toBeTruthy();
+            expect(scope.reflections[2].ticked).toBeTruthy();
         });
     });
 
@@ -91,9 +123,12 @@ describe("Word details controller spec:", function () {
     });
     describe("When fetching a given word via an ID,", function () {
         it("then should have the word's details, if the word exist", function () {
+
+            $httpBackend.expectGET("/api/words/edit?id=1").respond(test_word);
+
             $httpBackend.when("GET", "/api/people?role=Poet").respond({"people": "somePerson"});
             $httpBackend.when("GET", "/api/category/word").respond(null);
-            $httpBackend.when("GET", "/api/reflections/summary").respond(null);
+            $httpBackend.when("GET", "/api/reflections/summary").respond({"reflections":[]});
             $httpBackend.when("GET", "/api/people").respond({"people": ""});
             $httpBackend.when("GET", "/api/songs/getAllSongs").respond({
                 "songs": [{
@@ -102,20 +137,14 @@ describe("Word details controller spec:", function () {
                     "words": {"words": []}
                 }]
             });
-            var mockedId = 1;
-            spyOn($location, 'search').andReturn({id: mockedId});
-            $httpBackend.when("GET", "/api/words/edit?id=1").respond({
-                'id': 1,
-                'original': 'dummyOriginalText',
-                'english': 'dummyEnglishText'
-            });
 
             scope.init();
             $httpBackend.flush();
 
-            expect(scope.formInfo.original).toBe('dummyOriginalText');
+            expect(scope.formInfo.wordOriginal).toBe('अकथ कथा');
         });
         it("then should have singers as a comma separated entries for a song", function () {
+
             var mockedSongs = {
                 "songs": [{
                     "englishTransliterationTitle": "some title",
@@ -127,9 +156,11 @@ describe("Word details controller spec:", function () {
                     "words": {"words": []}
                 }]
             };
+            $httpBackend.expectGET("/api/words/edit?id=1").respond(test_word);
+
             $httpBackend.when("GET", "/api/people?role=Poet").respond({"people": "somePerson"});
             $httpBackend.when("GET", "/api/category/word").respond(null);
-            $httpBackend.when("GET", "/api/reflections/summary").respond(null);
+            $httpBackend.when("GET", "/api/reflections/summary").respond({"reflections":[]});
             $httpBackend.when("GET", "/api/people").respond({"people": ""});
             $httpBackend.when("GET", "/api/songs/getAllSongs").respond(mockedSongs);
 
@@ -147,9 +178,12 @@ describe("Word details controller spec:", function () {
                     "words": {"words": []}
                 }, {"englishTransliterationTitle": "some title2", "singers": [], "words": {"words": []}}]
             };
+
+            $httpBackend.expectGET("/api/words/edit?id=1").respond(test_word);
+
             $httpBackend.when("GET", "/api/people?role=Poet").respond({"people": "somePerson"});
             $httpBackend.when("GET", "/api/category/word").respond(null);
-            $httpBackend.when("GET", "/api/reflections/summary").respond(null);
+            $httpBackend.when("GET", "/api/reflections/summary").respond({"reflections":[]});
             $httpBackend.when("GET", "/api/people").respond({"people": ""});
             $httpBackend.when("GET", "/api/songs/getAllSongs").respond(mockedSongs);
 
@@ -161,3 +195,70 @@ describe("Word details controller spec:", function () {
         });
     });
 });
+
+var test_word = {
+    "id": 3,
+    "wordOriginal": "अकथ कथा",
+    "wordTranslation": "Untellable Tale",
+    "wordTransliteration": "Akath Katha",
+    "englishIntroExcerpt": "Akath means inexpressible story.",
+    "hindiIntroExcerpt": null,
+    "diacritic": null,
+    "isRootWord": false,
+    "showOnLandingPage": true,
+    "meaning": null,
+    "wordIntroductions": [],
+    "reflections": [
+        {
+            "id": 1,
+            "title": "Poet is God says Vipul",
+            "speaker": {
+                "id": 16,
+                "name": "Vipul Rikhi",
+                "hindiName": ""
+            }
+        },
+        {
+            "id": 5,
+            "title": "reflection",
+            "speaker": null
+        }
+    ],
+    "relatedWords": [],
+    "songs": [],
+    "synonyms": [],
+    "writers": [],
+    "people": []
+};
+
+var test_reflection_summaries ={
+    "reflections": [
+        {
+            "id": 4,
+            "title": "Unbelievable",
+            "speaker": {
+                "id": 14,
+                "name": "Gavra Devi",
+                "hindiName": ""
+            }
+        },
+        {
+            "id": 1,
+            "title": "Poet is God says Vipul",
+            "speaker": {
+                "id": 16,
+                "name": "Vipul Rikhi",
+                "hindiName": ""
+            }
+        },
+        {
+            "id": 5,
+            "title": "reflection",
+            "speaker": {
+                "id": 17,
+                "name": "Fakru",
+                "hindiName": ""
+            }
+        }
+    ]
+}
