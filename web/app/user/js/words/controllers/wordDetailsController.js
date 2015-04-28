129dab3019381e9daa00f1fc927acc2b43800f5d
indraner <indraner@INindraner.local>
2015-04-28 13:15:27 +0530

    #70 | fixing styling for the details section, added related links for both word and reflection | indraneel

--- web/app/user/js/words/controllers/wordDetailsController.js
+++ web/app/user/js/words/controllers/wordDetailsController.js
@@ -1,67 +1,7 @@
 angular.module("word").
     controller('wordDetailsController', ['$scope','wordService', '$route',function($scope,wordService, $route){
-      /*  $scope.popupService = popupService;
-        $scope.detailsService = $scope;
-        $scope.wordReflections = {"wordThumbnails": [], "reflections": []};
-        $scope.wordReflectionsCompleteInfo = {"wordCompleteInfo": [], "reflectionsCompleteInfo": []};
-        $scope.words = [];
-        $scope.showVersion = true;
-        $scope.numberOfReflections = 0;
-        $scope.wordId = '';
-        $scope.showReflection = false;
-        $scope.currentReflection = {};
-        $scope.currentWord = {};
-        $scope.showWord = true;
-
-        $scope.init = function () {
-            $scope.wordId = $routeParams.id;
-
-            wordService.getVersions($scope.wordId).then(function (result) {
-                $scope.words.push(result.data.word);
-                $scope.wordReflections.reflections = reflectionMapper.getThumbnails(result.data.reflections);
-                $scope.wordReflections.wordThumbnails = wordMapper.getThumbnails($scope.words, '');
-                $scope.wordReflectionsCompleteInfo.wordCompleteInfo = wordMapper.getWordCompleteInfo($scope.words);
-                $scope.currentWord = $scope.wordReflectionsCompleteInfo.wordCompleteInfo[0];
-                $scope.wordReflectionsCompleteInfo.reflectionsCompleteInfo = reflectionMapper.getReflectionsCompleteInfo(result.data.reflections);
-                $scope.numberOfReflections = $scope.wordReflections.reflections.length;
-            });
-
-        }();
-
-        $scope.openReflection = function (id) {
-            $scope.showReflection = true;
-            $scope.showWord = false;
-            angular.forEach($scope.wordReflectionsCompleteInfo.reflectionsCompleteInfo, function (reflection) {
-                if (reflection.id == id)
-                    $scope.currentReflection = reflection;
-            });
-
-        };
-
-        $scope.openWord = function (id) {
-            $scope.showWord = true;
-            $scope.showReflection = false;
-            angular.forEach($scope.wordReflectionsCompleteInfo.wordCompleteInfo, function (word) {
-                if (word.id == id)
-                    $scope.currentWord = word;
-            });
-
-        };
-
-        $scope.toggleVersion = function () {
-            $scope.showVersion = !$scope.showVersion;
-        };
-
-        $scope.shouldShowReflections = function () {
-            return $scope.wordReflections.reflections.length > 0;
-        };
-*/
-
-
-
         $scope.selectThumbnail = function(thumbnail){
             if(_.isEmpty(thumbnail)) return;
-            console.log("selected : ",thumbnail);
             if(thumbnail.type === 'word'){
                 $scope.detailsObject = new AjabShahar.DetailsObject($scope.wordDetails,thumbnail.type)
             }
@@ -90,7 +30,6 @@ angular.module("word").
                     )
                 )
             });
-
         }();
     }]);
 
