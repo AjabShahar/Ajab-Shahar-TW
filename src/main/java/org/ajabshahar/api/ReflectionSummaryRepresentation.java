b00519f14d32ba4d6d13b2621a74e3d5230ac4f8
Chanakya <chanakg@thoughtworks.com>
2015-05-06 11:53:33 +0530

    #209 Authoring thumbnail url for reflections [Chanakya]

--- src/main/java/org/ajabshahar/api/ReflectionSummaryRepresentation.java
+++ src/main/java/org/ajabshahar/api/ReflectionSummaryRepresentation.java
@@ -16,11 +16,12 @@ public class ReflectionSummaryRepresentation {
     private String soundCloudId;
     private String youtubeVideoId;
     private Set<ReflectionTranscript> reflectionTranscripts;
+    private String thumbnailUrl;
 
     public ReflectionSummaryRepresentation() {
     }
 
-    public ReflectionSummaryRepresentation(long id, String title, PersonSummaryRepresentation speaker, Boolean published, String youtubeVideoId, String soundCloudId, Set<ReflectionTranscript> reflectionTranscripts) {
+    public ReflectionSummaryRepresentation(long id, String title, PersonSummaryRepresentation speaker, Boolean published, String youtubeVideoId, String soundCloudId, Set<ReflectionTranscript> reflectionTranscripts, String thumbnailUrl) {
         this.id = id;
         this.title = title;
         this.speaker = speaker;
@@ -28,6 +29,7 @@ public class ReflectionSummaryRepresentation {
         this.youtubeVideoId = youtubeVideoId;
         this.soundCloudId = soundCloudId;
         this.reflectionTranscripts = reflectionTranscripts;
+        this.thumbnailUrl = thumbnailUrl;
     }
 
     @JsonProperty("id")
@@ -98,7 +100,7 @@ public class ReflectionSummaryRepresentation {
 
     public static ReflectionSummaryRepresentation createFrom(Reflection reflection){
         if(reflection != null){
-            return new ReflectionSummaryRepresentation(reflection.getId(),reflection.getTitle(),PersonSummaryRepresentation.createFrom(reflection.getSpeaker()),reflection.getIsAuthoringComplete(), reflection.getYoutubeVideo(), reflection.getSoundCloudId(), reflection.getReflectionTranscripts());
+            return new ReflectionSummaryRepresentation(reflection.getId(),reflection.getTitle(),PersonSummaryRepresentation.createFrom(reflection.getSpeaker()),reflection.getIsAuthoringComplete(), reflection.getYoutubeVideo(), reflection.getSoundCloudId(), reflection.getReflectionTranscripts(), reflection.getThumbnailURL() );
         }
         return null;
     }
