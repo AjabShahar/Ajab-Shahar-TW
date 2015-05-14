dc6c10004f6935d5ed322cc87176a71bce3531fb
padma <padmavb@thoughtworks.com>
2015-05-14 15:18:36 +0530

    Fixed bug, authoring thumbnail url for reflection

--- web/test/unit/admin/controllers/reflectionDetailsControllerSpec.js
+++ web/test/unit/admin/controllers/reflectionDetailsControllerSpec.js
@@ -12,13 +12,10 @@ describe("reflection details controller", function () {
         fakeWindow = {location: {href: ''}};
         $cookies.user = "admin";
 
-        spyOn($location, 'search').andReturn({id:1});
-
-
         _$controller_('reflectionDetailsController', {
             $scope: scope,
             $window: fakeWindow,
-            $location: _$location_,
+            $location: $location,
             reflectionContentService: reflectionContentService,
             loginVerifyService: loginVerifyService
         });
@@ -26,43 +23,72 @@ describe("reflection details controller", function () {
         httpBackend.expectGET('/api/people/summary').respond(200, ['people data']);
         httpBackend.expectGET('/api/words/summary').respond(200, test_admin_wordSummary);
         httpBackend.expectGET('/api/songs/getAllSongs').respond(200, {data: null});
-        httpBackend.expectGET('/api/reflections/edit?id=1').respond(200,test_admin_reflection);
     }));
 
     describe("should test save of reflection", function () {
         it("after saving should redirect to admin page", function () {
             httpBackend.flush();
-            httpBackend.expectPOST('/api/reflections').respond(200);
+            httpBackend.when('POST','/api/reflections').respond(200);
 
             scope.saveData();
             httpBackend.flush();
 
             expect(fakeWindow.location.href).toBe('/admin/partials/home.html');
         });
+
+        it("should append /images/ for thumbnail url,if it is just filename", function () {
+            scope.reflection.thumbnailURL = "thumbnail.jpg";
+            httpBackend.when('POST','/api/reflections').respond(200);
+
+            scope.saveData();
+            httpBackend.flush();
+
+            expect(scope.reflection.thumbnailURL).toBe('/images/thumbnail.jpg');
+        });
+        it("should not append /images/ for thumbnail url,if it already have /images appended in filename", function () {
+            scope.reflection.thumbnailURL = "/images/thumbnail.jpg";
+            httpBackend.when('POST','/api/reflections').respond(200);
+
+            scope.saveData();
+            httpBackend.flush();
+
+            expect(scope.reflection.thumbnailURL).toBe('/images/thumbnail.jpg');
+        });
+        it("should not append /images/ for thumbnail url,if the image is from internet source", function () {
+            scope.reflection.thumbnailURL = "http://www.hdwallpapersimages.com/wp-content/uploads/images/Child-Girl-with-Sunflowers-Images.jpg";
+            httpBackend.when('POST','/api/reflections').respond(200);
+
+            scope.saveData();
+            httpBackend.flush();
+
+            expect(scope.reflection.thumbnailURL).toBe('http://www.hdwallpapersimages.com/wp-content/uploads/images/Child-Girl-with-Sunflowers-Images.jpg');
+        });
     });
 
-    describe("Should remove other reflection types and their information when one of the types is selected", function(){
-       it("When reflection type is audio, should have other types as null", function(){
-           httpBackend.flush();
-           httpBackend.expectPOST('/api/reflections').respond(200, {'id': 1});
+    describe("Should remove other reflection types and their information when one of the types is selected", function () {
 
-           scope.saveData();
-           httpBackend.flush();
-
-           expect(scope.reflection.type).toBe('video');
-
-           scope.reflection.type = "audio";
-           scope.reflection.soundCloudId = "soundCloudURL";
-
-           scope.saveData();
-
-           expect(scope.reflection.about).toBe("about");
-           expect(scope.reflection.youtubeVideoId).toBe(null);
-           expect(scope.reflection.soundCloudId).toBe("soundCloudURL");
-       }) ;
-        it("When reflection type is audio, should have other types as null", function(){
+        it("When reflection type is audio, should have other types as null", function () {
             httpBackend.flush();
-            httpBackend.expectPOST('/api/reflections').respond(200, {'id': 1});
+            httpBackend.when('POST','/api/reflections').respond(200, {'id': 1});
+
+            scope.saveData();
+            httpBackend.flush();
+
+            //expect(scope.reflection.type).toBe('video');
+
+            scope.reflection.type = "audio";
+            scope.reflection.soundCloudId = "soundCloudURL";
+            scope.reflection.about = "about";
+
+            //scope.saveData();
+
+            expect(scope.reflection.about).toBe("about");
+            expect(scope.reflection.youtubeVideoId).toBe(null);
+            expect(scope.reflection.soundCloudId).toBe("soundCloudURL");
+        });
+        it("When reflection type is audio, should have other types as null", function () {
+            httpBackend.flush();
+            httpBackend.when('POST','/api/reflections').respond(200, {'id': 1});
 
             scope.saveData();
             httpBackend.flush();
@@ -76,105 +102,105 @@ describe("reflection details controller", function () {
             expect(scope.reflection.youtubeVideoId).toBe(null);
             expect(scope.reflection.soundCloudId).toBe(null);
             expect(scope.reflection.type).toBe("text");
-        }) ;
+        });
     });
-
     describe("should test updating of reflection", function () {
         it("after updating should redirect to admin page", function () {
+            spyOn($location, 'search').andReturn({id: 1});
+            httpBackend.when('GET','/api/reflections/edit?id=1').respond(test_admin_reflection);
+
             httpBackend.flush();
-            httpBackend.expectPOST('/api/reflections').respond(200, {'id': 1});
+            httpBackend.when('POST','/api/reflections').respond(200, {'id': 1});
 
             scope.saveData();
             httpBackend.flush();
 
             expect(fakeWindow.location.href).toBe('/admin/partials/home.html');
         });
+        it("should show already linked words for the reflection", function () {
+            spyOn($location, 'search').andReturn({id: 1});
+            httpBackend.when('GET','/api/reflections/edit?id=1').respond(test_admin_reflection);
+
+            httpBackend.flush();
+
+            expect(scope.words[0].ticked).toBeTruthy();
+            expect(scope.words[1].ticked).toBeFalsy();
+        });
     });
 
-    it("should show already linked words for the reflection",function(){
-        httpBackend.flush();
-
-        expect(scope.words[0].ticked).toBeTruthy();
-        expect(scope.words[2].ticked).toBeTruthy();
-
-    })
 });
 
 
 var test_admin_wordSummary = [
-        {
-            "id": 1,
-            "wordOriginal": "अकथ कथा",
-            "wordTranslation": "Akath Katha",
-            "wordTransliteration": "Untellable Tale",
-            "hindiIntroExcerpt": "Akath means inexpressible, indescribable, ineffable. Beyond words. Beyond language. Beyond mind. But why all the hue and cry if the tale cannot be told?",
-            "englishIntroExcerpt": "",
-            "writers": [],
-            "rootWord": false
-        },
-        {
-            "id": 2,
-            "wordOriginal": "जागना सोना",
-            "wordTranslation": "Waking Sleeping",
-            "wordTransliteration": "Jaagna Sona",
-            "hindiIntroExcerpt": "A story tells of how, when the Buddha attained illumination, somebody asked him: ‘So, are you enlightened now?’ The Buddha’s response was: ‘I am awake.’",
-            "englishIntroExcerpt": "",
-            "writers": [],
-            "rootWord": true
-        },
-        {
-            "id": 3,
-            "wordOriginal": "अकथ कथा",
-            "wordTranslation": "Untellable Tale",
-            "wordTransliteration": "Akath Katha",
-            "hindiIntroExcerpt": "Akath means inexpressible, indescribable, ineffable. Beyond words. Beyond language. Beyond mind. But why all the hue and cry if the tale cannot be told? Perhaps because there is an enticing, irresistible vastness and mysteriousness to the unspeakable story. It is not mystification but an invitation to enter… a realm of experience beyond description.",
-            "englishIntroExcerpt": "",
-            "writers": [],
-            "rootWord": false
-        },
-        {
-            "id": 4,
-            "wordOriginal": "शून्य",
-            "wordTranslation": "Emptiness",
-            "wordTransliteration": "Shoonya",
-            "hindiIntroExcerpt": "Shoonya is literally zero in the Indian numbering system, the place from which all things begin. It is also a technical term in Buddhism, usually translated as emptiness or nothingness. In Sanskrit, one of its primary meanings is empty space, or void.",
-            "englishIntroExcerpt": "",
-            "writers": [],
-            "rootWord": false
-        }
+    {
+        "id": 1,
+        "wordOriginal": "अकथ कथा",
+        "wordTranslation": "Akath Katha",
+        "wordTransliteration": "Untellable Tale",
+        "hindiIntroExcerpt": "Akath means inexpressible, indescribable, ineffable. Beyond words. Beyond language. Beyond mind. But why all the hue and cry if the tale cannot be told?",
+        "englishIntroExcerpt": "",
+        "writers": [],
+        "rootWord": false
+    },
+    {
+        "id": 2,
+        "wordOriginal": "जागना सोना",
+        "wordTranslation": "Waking Sleeping",
+        "wordTransliteration": "Jaagna Sona",
+        "hindiIntroExcerpt": "A story tells of how, when the Buddha attained illumination, somebody asked him: ‘So, are you enlightened now?’ The Buddha’s response was: ‘I am awake.’",
+        "englishIntroExcerpt": "",
+        "writers": [],
+        "rootWord": true
+    },
+    {
+        "id": 3,
+        "wordOriginal": "अकथ कथा",
+        "wordTranslation": "Untellable Tale",
+        "wordTransliteration": "Akath Katha",
+        "hindiIntroExcerpt": "Akath means inexpressible, indescribable, ineffable. Beyond words. Beyond language. Beyond mind. But why all the hue and cry if the tale cannot be told? Perhaps because there is an enticing, irresistible vastness and mysteriousness to the unspeakable story. It is not mystification but an invitation to enter… a realm of experience beyond description.",
+        "englishIntroExcerpt": "",
+        "writers": [],
+        "rootWord": false
+    },
+    {
+        "id": 4,
+        "wordOriginal": "शून्य",
+        "wordTranslation": "Emptiness",
+        "wordTransliteration": "Shoonya",
+        "hindiIntroExcerpt": "Shoonya is literally zero in the Indian numbering system, the place from which all things begin. It is also a technical term in Buddhism, usually translated as emptiness or nothingness. In Sanskrit, one of its primary meanings is empty space, or void.",
+        "englishIntroExcerpt": "",
+        "writers": [],
+        "rootWord": false
+    }
 ];
 
 var test_admin_reflection = {
+
     "showOnMainFcPage": true,
+    "thumbnailURL": "https://avatars1.githubusercontent.com/u/3875065?v=3&s=72",
+    "info": "<p>By Vipul</p>",
+    "about": null,
+    "duration": "",
+    "reflectionExcerpt": "This is text reflection excerpt. This is text reflection excerpt.This is text reflection excerpt",
     "id": 1,
-    "title": "Poet is God says Vipul",
-    "verb": "by",
+    "title": "Poet is God",
+    "verb": "says",
     "speaker": {
         "id": 16,
         "name": "Vipul Rikhi",
-        "hindiName": ""
+        "hindiName": "",
+        "primaryOccupation": "Singer"
     },
     "soundCloudId": null,
     "youtubeVideoId": "MtIoD16yTQc",
-    "transcripts": [
+    "reflectionTranscripts": [
         {
             "id": 1,
             "hindiTranscript": null,
             "englishTranscript": null
         }
     ],
-    "about": "about",
     "words": [
-        {
-            "id": 3,
-            "wordOriginal": "अकथ कथा",
-            "wordTranslation": "Untellable Tale",
-            "wordTransliteration": "Akath Katha",
-            "hindiIntroExcerpt": "Akath means inexpressible, indescribable, ineffable. Beyond words. Beyond language. Beyond mind. But why all the hue and cry if the tale cannot be told? Perhaps because there is an enticing, irresistible vastness and mysteriousness to the unspeakable story. It is not mystification but an invitation to enter… a realm of experience beyond description.",
-            "englishIntroExcerpt": "",
-            "writers": [],
-            "rootWord": false
-        },
         {
             "id": 1,
             "wordOriginal": "अकथ कथा",
@@ -186,5 +212,39 @@ var test_admin_reflection = {
             "rootWord": false
         }
     ],
-    "publish": true
+    "songs": [
+        {
+            "id": 11,
+            "englishTranslationTitle": "Everyday I Ask For Your Well Being",
+            "englishTransliterationTitle": "Nit Khair Manga",
+            "singers": [
+                {
+                    "id": 25,
+                    "name": "Mukhtiyar Ali",
+                    "hindiName": "",
+                    "primaryOccupation": null
+                }
+            ],
+            "poet": [
+                {
+                    "id": 26,
+                    "name": "Badar Muneer",
+                    "hindiName": "",
+                    "primaryOccupation": null
+                }
+            ],
+            "duration": "07:45",
+            "category": "Songs",
+            "thumbnailUrl": "https://farm8.staticflickr.com/7583/16097980187_72dfa07068_o.png"
+        }
+    ],
+    "publish": true,
+    "people": [
+        {
+            "id": 8,
+            "name": "Roshik",
+            "hindiName": "",
+            "primaryOccupation": null
+        }
+    ]
 };
