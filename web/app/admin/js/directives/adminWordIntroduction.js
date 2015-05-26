63ed372166a8b7dbde3d51d12868e1a32a90635c
Jaideep Rana <jaideeprana45@yahoo.com>
2015-05-26 11:49:52 +0530

    Fixng the logic in word-introduction for adding a word. <Jaideep>

--- web/app/admin/js/directives/adminWordIntroduction.js
+++ web/app/admin/js/directives/adminWordIntroduction.js
@@ -18,16 +18,15 @@ wordsAdminApp.directive("adminWordIntroduction", function () {
                 $scope.wordIntroductions = $scope.wordIntroductions.length == 0 ? [] : $scope.wordIntroductions;
                 var introTextOriginals = $scope.wordIntroHindi != null ? $scope.wordIntroHindi.split(/\n{2,}/g) : [];
                 var introTextTranslations = $scope.wordIntroEnglish != null ? $scope.wordIntroEnglish.split(/\n{2,}/g) : [];
-                var total = introTextOriginals.length > 0 ? introTextOriginals.length : introTextTranslations.length;
+                var total = Math.max(introTextOriginals.length, introTextTranslations.length);
                 for (var i = 0; i < total; i++) {
                     var wordIntroduction = {};
                     wordIntroduction.wordIntroHindi = introTextOriginals.length > 0 ? "<p>" + introTextOriginals[i].split("\n").join("<br />") + "</p>" : "";
                     wordIntroduction.wordIntroEnglish = introTextTranslations.length > 0 ? "<p>" + introTextTranslations[i].split("\n").join("<br />") + "</p>" : "";
                     $scope.wordIntroductions.push(wordIntroduction);
                 }
-                $scope.wordIntroduction = "";
-                $scope.wordIntroEnglish = "";
-                $scope.wordIntroHindi = "";
+                $scope.wordIntroEnglish = null;
+                $scope.wordIntroHindi = null;
             };
         }
     };
