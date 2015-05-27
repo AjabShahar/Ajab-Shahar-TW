761ab13f7597ea57359fc203017613d4733f57fa
padma <padmavb@thoughtworks.com>
2015-05-27 18:21:53 +0530

    Added unit test for person details controller on admin side

--- web/app/admin/js/controllers/person/personDetailsController.js
+++ web/app/admin/js/controllers/person/personDetailsController.js
@@ -1,15 +1,13 @@
 personAdminApp.controller('personDetailsController', ['$scope', '$http', '$window', '$location', 'contentService', 'loginVerifyService',
     function ($scope, $http, $window, $location, contentService, loginVerifyService) {
         loginVerifyService.redirectIfNotAuthenticated();
-        $scope.pageHeading = "";
-        $scope.pageTitle = "";
         $scope.formInfo = {};
         $scope.categoryList = [];
         $scope.init = function () {
             contentService.getAllCategories('person').success(function (result) {
                 $scope.categoryList = result;
                 $scope.primaryOccupationsList = angular.copy(result);
-                getPersonData();
+                getPersonDetails();
             });
         };
 
@@ -20,45 +18,29 @@ personAdminApp.controller('personDetailsController', ['$scope', '$http', '$windo
         };
 
         $scope.saveData = function () {
-            //if ($scope.formInfo.primaryOccupation == null || ($scope.formInfo.primaryOccupationId != $scope.formInfo.primaryOccupation.id)) {
-            //    $scope.formInfo.primaryOccupation = _.findWhere($scope.categoryList, {"id": $scope.formInfo.primaryOccupationId});
-            //}
-
             if ($scope.formInfo.thumbnailURL && $scope.formInfo.thumbnailURL.indexOf("http") === -1 && $scope.formInfo.thumbnailURL.indexOf("/images/") === -1) {
                 $scope.formInfo.thumbnailURL = '/images/' + $scope.formInfo.thumbnailURL;
             }
             savePerson();
         };
 
-        var getPersonDetails = function (personId) {
-            $http.get('/api/people/' + personId).success(function (data) {
-                $scope.formInfo = data;
+        var getPersonDetails = function () {
+            var personId = $location.search().id;
+            if(personId){
+                $http.get('/api/people/' + personId).success(function (data) {
+                    $scope.formInfo = data;
 
-                angular.forEach(data.roles, function (selectedCategoryName) {
-                    angular.forEach($scope.categoryList, function (category) {
-                        if (!category.ticked) {
-                            category.ticked = (selectedCategoryName === category.name)
-                        }
+                    angular.forEach(data.roles, function (selectedCategoryName) {
+                        angular.forEach($scope.categoryList, function (category) {
+                            if (!category.ticked) {
+                                category.ticked = (selectedCategoryName === category.name)
+                            }
+                        });
                     });
                 });
-
-                $scope.formInfo.primaryOccupationId = Boolean($scope.formInfo.primaryOccupation != null) ? $scope.formInfo.primaryOccupation.id : '';
-            });
-        };
-
-        var getPersonData = function () {
-            var personId = $location.search().id;
-
-            if (!personId) {
-                $scope.pageTitle = "Person details - admin";
-                $scope.pageHeading = "Add Person details";
-            }
-            else {
-                $scope.pageTitle = "Edit person details";
-                $scope.pageHeading = "Edit Person details";
-                getPersonDetails(personId);
             }
         };
+
         $scope.$watch("formInfo.primaryOccupation", function (newValue) {
             $scope.categoryList = _.reject($scope.categoryList, function (categoty) {
                 return !_.isEmpty(newValue) && categoty.id === newValue.id;
