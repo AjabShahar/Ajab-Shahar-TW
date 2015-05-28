6fea347b9f3e52720bcc513a27dd9af757a8f178
indraner <indraner@INindraner.local>
2015-05-28 14:52:31 +0530

    refactoring - removing unwanted lines of code, links to unused files in index.htmls | indraneel

--- web/app/user/js/common/directives/heightOffset/heightOffset.js
+++ web/app/user/js/common/directives/heightOffset/heightOffset.js
@@ -1,20 +1,20 @@
 "use strict";
 
-angular.module('utilities').directive('heightOffset',function(){
+angular.module('utilities').directive('heightOffset', function () {
     return {
-        scope:{},
-        link:function(scope,element,attrs){
+        scope: {},
+        link: function (scope, element, attrs) {
             var offset = parseInt(attrs.heightOffset);
             $(element).height($(window).height() + offset);
         }
     }
 });
 
-angular.module('utilities').directive('ellipsis',['$timeout',function($timeout){
+angular.module('utilities').directive('ellipsis', ['$timeout', function ($timeout) {
     return {
-        scope:{},
-        link:function(scope,element,attrs){
-            $timeout(function(){
+        scope: {},
+        link: function (scope, element, attrs) {
+            $timeout(function () {
                 $(element).readmore({
                     moreLink: '<a href="#" class="read-link">see more</a>',
                     lessLink: '<a href="#" class="read-link">see less</a>'
@@ -24,19 +24,3 @@ angular.module('utilities').directive('ellipsis',['$timeout',function($timeout){
         }
     }
 }]);
-
-angular.module('utilities').directive('mosaic',function(){
-    return {
-        scope:{},
-        link:function(scope,element,attrs){
-            $(element).masonry({
-                columnWidth: ".media",
-                itemSelector: '.media',
-                percentPosition: true,
-                isOriginLeft: true
-
-
-            });
-        }
-    }
-});
