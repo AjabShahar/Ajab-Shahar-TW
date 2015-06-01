64d22a39f61bd4ba1b32d0ae3ff1293e88c4c2c7
Jaideep Rana <jaideeprana45@yahoo.com>
2015-06-01 14:00:31 +0530

    Fixing broken QA. <Jaideep>

--- src/main/java/org/ajabshahar/api/common/BaseRepresentation.java
+++ src/main/java/org/ajabshahar/api/common/BaseRepresentation.java
@@ -2,17 +2,20 @@ package org.ajabshahar.api.common;
 
 import org.ajabshahar.api.PersonSummaryRepresentation;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class BaseRepresentation {
     public Set<PersonSummaryRepresentation> getOnlyPublishedPeople(Set<PersonSummaryRepresentation> people){
+        Set<PersonSummaryRepresentation> publishedPeople = new LinkedHashSet<>();
         if(people != null) {
             for (PersonSummaryRepresentation person : people) {
-                if (!person.isPublish()) {
-                    people.remove(person);
+                if (person.isPublish()) {
+                    publishedPeople.add(person);
                 }
             }
         }
-        return people;
+        return publishedPeople;
     }
 }
