77211e27351a373a048073a728301cb8b1d1b712
Chanakya <chanakg@thoughtworks.com>
2015-06-04 23:04:11 +0530

    sorting data on glossary page [Chanakya]

--- web/app/user/js/words/controllers/glossaryController.js
+++ web/app/user/js/words/controllers/glossaryController.js
@@ -1,8 +1,12 @@
-angular.module("word").controller('glossaryController', ['$scope', 'wordService', function ($scope, wordService) {
+angular.module("word").controller('glossaryController', ['$scope', 'wordService',  '$filter', function ($scope, wordService,$filter) {
+
+    var sortList = function (list, sortCriteria) {
+        return $filter('orderBy')(list, sortCriteria);
+    };
 
     $scope.init = function () {
         wordService.getGlossaryPageContent().then(function (result) {
-            $scope.glossary = result.data.words;
+            $scope.glossary = sortList(result.data.words, 'wordTransliteration');
             $scope.numberOfTerms = $scope.glossary.length;
         });
     };
