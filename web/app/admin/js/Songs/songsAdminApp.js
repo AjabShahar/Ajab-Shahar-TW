f714066f2fbaf5ca8bdf111d97313dd735a252d3
indraner <indraner@INindraner.local>
2015-06-09 14:48:38 +0530

    #163 | admin front end changes for adding the couplet and refrain buttons on Ta-toolbar | indraneel

--- web/app/admin/js/Songs/songsAdminApp.js
+++ web/app/admin/js/Songs/songsAdminApp.js
@@ -1,10 +1,43 @@
 var songsAdminApp = angular.module('songsAdminApp', ['multi-select', 'textAngular', 'commonApp', 'adminCommon']);
 songsAdminApp.factory('songContentService', ['$http', songContentService]);
 
-songsAdminApp.config(function ($locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-    });
+songsAdminApp.config(function ($locationProvider,$provide) {
+    $locationProvider.html5Mode({enabled: true});
+
+    //var rangy = rangy || {};
+    if(!_.isEmpty(rangy)){
+        $provide.decorator('taOptions', ['taRegisterTool', '$delegate', '$log',
+            function(taRegisterTool, taOptions, $log){
+                rangy.init();
+                coupletApplier = rangy.createClassApplier("initial-couplets", {normalize: true});
+                taRegisterTool('couplet', {
+                    iconclass: "fa fa-comment",
+                    buttontext: "couplet",
+                    action: function(deferred, restoreSelection){
+                        return coupletApplier.toggleSelection();
+                    },
+                    activeState: function(commonElement) {
+                        return coupletApplier.isAppliedToSelection();
+                    }
+                });
+                taOptions.toolbar[1].push('couplet');
+
+                refrainApplier = rangy.createClassApplier("refrain", {normalize: true});
+                taRegisterTool('refrain', {
+                    iconclass: "fa fa-ellipsis-v",
+                    buttontext: "refrain",
+                    action: function(deferred, restoreSelection){
+                        return refrainApplier.toggleSelection();
+                    },
+                    activeState: function(commonElement) {
+                        return refrainApplier.isAppliedToSelection();
+                    }
+                });
+                taOptions.toolbar[1].push('refrain');
+                return taOptions;
+            }]);
+    }
+
 });
 
 songsAdminApp.constant("PAGES", {
