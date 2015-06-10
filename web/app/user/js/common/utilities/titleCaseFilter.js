4211bed038a2435be1d44eadeb08bf5d4bc17ae1
Sakunthala <sakuntha@thoughtworks.com>
2015-06-10 18:12:26 +0530

    Added title case filter and applied in related links lightbox

---
+++ web/app/user/js/common/utilities/titleCaseFilter.js
@@ -0,0 +1,7 @@
+angular.module('utilities', [])
+  .filter('titleCase', function() {
+    return function(input) {
+      input = input || '';
+      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    };
+  });
