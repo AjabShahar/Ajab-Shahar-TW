37e299006a28e61e0902b0f5ada4976d282c8e74
Jaideep Rana <jaideeprana45@yahoo.com>
2015-06-17 17:21:34 +0530

    Written configs for social-share. <Jaideep>

--- web/app/user/static/sharingSong.php
+++ web/app/user/static/sharingSong.php
@@ -1,12 +1,13 @@
 <?php
-$SITE_ROOT = $localhost;
+$SITE_ROOT = "http://localhost";
 
 $jsonData = getData($SITE_ROOT);
 makePage($jsonData, $SITE_ROOT);
 function getData($siteRoot) {
     $id = ctype_digit($_GET['id']) ? $_GET['id'] : 1;
-    $rawData = file_get_contents($siteRoot.'api/songs'.$id);
-    return json_decode($rawData);
+    $rawData = http_get($siteRoot.'/api/songs/'.$id, array('headers' => array('content_type' => 'application/json')), $info);
+    $res =  http_parse_message($rawData)->body;
+    return json_decode($res);
 }
 
 function makePage($data, $siteRoot) {
@@ -26,4 +27,3 @@ function makePage($data, $siteRoot) {
     </html>
 <?php
 }
-?>
