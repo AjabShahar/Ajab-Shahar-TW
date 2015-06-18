398e2f25d01b1939afd6be8c753de82cde5a1aab
Jaideep Rana <jaideeprana45@yahoo.com>
2015-06-18 11:38:57 +0530

    Added sharing files for word and reflection. <Jaideep>

---
+++ web/app/user/static/sharingReflection.php
@@ -0,0 +1,27 @@
+<?php
+$SITE_ROOT = "http://localhost";
+
+$jsonData = getData($SITE_ROOT);
+makePage($jsonData, $SITE_ROOT);
+function getData($siteRoot) {
+    $id = ctype_digit($_GET['id']) ? $_GET['id'] : 1;
+    $rawData = http_get($siteRoot.'/api/reflections/edit?id='.$id, array('headers' => array('content_type' => 'application/json')), $info);
+    $res =  http_parse_message($rawData)->body;
+    return json_decode($res);
+}
+
+function makePage($data, $siteRoot) {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta property="og:title" content="<?php echo $data->title; ?>" />
+            <meta property="og:description" content="<?php echo $data->about; ?>" />
+            <meta property="og:image" content="<?php echo $data->thumbnailURL; ?>" />
+
+        </head>
+        <body>
+        </body>
+    </html>
+<?php
+}
