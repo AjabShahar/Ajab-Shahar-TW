601e6f01cde0a8788b3ff6e0abaaba0c9a60020e
Sakunthala <sakuntha@thoughtworks.com>
2015-06-30 15:55:52 +0530

    Fixed the left nav scrolling position in song explore page

--- web/app/user/js/common/directives/animation/animationModule.js
+++ web/app/user/js/common/directives/animation/animationModule.js
@@ -6,12 +6,14 @@ animationModule.directive("scroll", function ($window) {
     return function (scope, element, attrs) {
         angular.element($window).bind("scroll", function () {
             var containerTop = $('.main-content').offset().top;
+            var footerTop = $('.footer-container').offset().top;
             var windowTop = $(window).scrollTop();
-            if (windowTop > containerTop) {
+            var position = (windowTop - containerTop)+ 50
+            if (windowTop > containerTop &&  (footerTop - position) > 350) {
 
                 $('.left-nav').css('top', (windowTop - containerTop)+ 50);
             }
-            else {
+            else if (footerTop - position > 350){
                 $('.left-nav').css('top', -5);
             }
 
