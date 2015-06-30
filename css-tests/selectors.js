fb1c416d018c0d1831e99456ab6cb1c7a3a8e36b
padma <padmavb@thoughtworks.com>
2015-06-30 15:27:13 +0530

    Removing unused files from repo

--- css-tests/selectors.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "words golu size": "html.ng-scope > body > div.ng-scope > div.words-details.header-with-carousel.details.ng-scope > div.page-header > div.ajab-wrapper > div.logo-container > a",
-    "words golu font": "html.ng-scope > body > div.ng-scope > div.words-details.header-with-carousel.details.ng-scope > div.page-header > div.ajab-wrapper > div.logo-container > a > span.page-logo",
-    "main header translit font": "html.ng-scope > body > div.ng-scope > div.words-details.header-with-carousel.details.ng-scope > div.page-header > div.ajab-wrapper > div.page-title > span.main.ng-binding",
-    "main header translation": "html.ng-scope > body > div.ng-scope > div.words-details.header-with-carousel.details.ng-scope > div.page-header > div.ajab-wrapper > div.page-title > span.sub-main.ng-binding"
-}
